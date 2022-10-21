c93dc28d9a4605e857bf7e24802841d8c4933c1b
yanglongqi <yanglongqi@xiaomi.com>
2022-10-21 13:12:47 +0800

    [复习][29]两数相除

--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,43 @@
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import java.util.ArrayList;
 
 class Solution {
 
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode p = head;
-        int t = k;
-        while (t-- > 0) {
-            if (p == null) {
-                return head;
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
             }
-            p = p.next;
+            if (divisor == 1) {
+                return dividend;
+            }
         }
 
-        ListNode newHead = reverse(head, p);
-        head.next = reverseKGroup(p, k);
-        return newHead;
+        boolean isNegative = (((dividend ^ divisor) >>> 31) & 1) == 1;
 
-    }
-
-    public ListNode reverse(ListNode head, ListNode end) {
-        ListNode pre = null;
-        ListNode curr = head;
-        ListNode next;
-        while (curr != end) {
-            next = curr.next;
-            curr.next = pre;
-            pre = curr;
-            curr = next;
+        if (dividend > 0) {
+            dividend = -dividend;
         }
-        return pre;
+
+        if (divisor > 0) {
+            divisor = -divisor;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int res = 1;
+        list.add(divisor);
+        while (dividend - list.get(list.size() - 1) <= list.get(list.size() - 1)) {
+            list.add(list.get(list.size() - 1) + list.get(list.size() - 1));
+            res <<= 1;
+        }
+
+        int ans = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (dividend <= list.get(i)) {
+                dividend -= list.get(i);
+                ans += res;
+            }
+            res >>= 1;
+        }
+        return isNegative ? -ans : ans;
     }
 }
