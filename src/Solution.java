b485712eedf274539b9fb627ece5162cb4b122a8
yanglongqi <yanglongqi@xiaomi.com>
2022-10-24 12:28:10 +0800

    [复习][23]合并K个升序链表

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,44 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
 
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int mid;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (target == nums[mid]) {
-                return mid;
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode();
+        ListNode p = head;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(value -> value.val));
+        for (ListNode list : lists) {
+            if (list != null) {
+                queue.add(list);
             }
-
-            if (nums[mid] >= nums[left]) {
-                if (target < nums[mid] && target >= nums[left]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-
         }
-        return -1;
+
+        while (!queue.isEmpty()) {
+            ListNode tList = queue.remove();
+            p.next = tList;
+            p = p.next;
+            if (tList.next != null) {
+                queue.add(tList.next);
+            }
+        }
+        return head.next;
     }
 }
