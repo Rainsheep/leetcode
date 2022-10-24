fe33c8606724857ac3e462bbe184655258c863af
yanglongqi <yanglongqi@xiaomi.com>
2022-10-24 12:32:38 +0800

    [复习][24]两两交换链表中的结点

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,3 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
 class ListNode {
 
@@ -21,24 +19,14 @@ class ListNode {
 
 class Solution {
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = new ListNode();
-        ListNode p = head;
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(value -> value.val));
-        for (ListNode list : lists) {
-            if (list != null) {
-                queue.add(list);
-            }
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
 
-        while (!queue.isEmpty()) {
-            ListNode tList = queue.remove();
-            p.next = tList;
-            p = p.next;
-            if (tList.next != null) {
-                queue.add(tList.next);
-            }
-        }
-        return head.next;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
     }
 }
