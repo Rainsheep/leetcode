cbc0a671f98dbf7223ced7c9fbf03ca34a894ef6
yanglongqi <yanglongqi@xiaomi.com>
2022-10-25 19:15:53 +0800

    [40]组合总和 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,50 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
 
-    List<List<Integer>> ans = new ArrayList<>();
+    HashSet<List<Integer>> ans = new HashSet<>();
     LinkedList<Integer> list = new LinkedList<>();
+    HashSet<Integer> set = new HashSet<>();
+    int[] nums = new int[100];
+    ArrayList<Integer> numList;
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        dfs(candidates, 0, target, 0);
-        return ans;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        for (int candidate : candidates) {
+            nums[candidate]++;
+            set.add(candidate);
+        }
+        numList = new ArrayList<>(set);
+
+        dfs(0, target, 0);
+        return new ArrayList<>(ans);
     }
 
-    private void dfs(int[] candidates, int i, int target, int sum) {
+    private void dfs(int i, int target, int sum) {
         if (sum == target) {
             ans.add(new ArrayList<>(list));
             return;
         }
 
-        if (sum > target || i >= candidates.length) {
+        if (sum > target || i >= numList.size()) {
             return;
         }
 
-        list.add(candidates[i]);
-        dfs(candidates, i, target, sum + candidates[i]);
-        list.removeLast();
+        int val = numList.get(i);
 
-        dfs(candidates, i + 1, target, sum);
+        for (int j = 0; j <= nums[val]; j++) {
+            for (int k = 0; k < j; k++) {
+                list.add(val);
+            }
+
+            dfs(i + 1, target, sum + val * j);
+
+            for (int k = 0; k < j; k++) {
+                list.removeLast();
+            }
+        }
     }
+
 }
