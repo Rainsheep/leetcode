a39e005a06cc6996df8a940bb8d7a54c4466c3cd
yanglongqi <yanglongqi@xiaomi.com>
2022-10-25 14:18:25 +0800

    [35]搜索插入位置

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,18 @@
 class Solution {
 
-    public int[] searchRange(int[] nums, int target) {
-        return new int[]{find(nums, target, true), find(nums, target, false)};
-    }
-
-    public int find(int[] nums, int target, boolean isLower) {
+    public int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length - 1, mid;
-        int ans = -1;
         while (left <= right) {
             mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                ans = mid;
+                return mid;
             }
-            if (nums[mid] > target || nums[mid] == target && isLower) {
+            if (nums[mid] > target) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return left;
     }
 }
