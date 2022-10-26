602e27b56fb8ba6aa37910889e12167514f6b85e
yanglongqi <yanglongqi@xiaomi.com>
2022-10-26 10:06:31 +0800

    [34]在排序数组中查找元素的第一个和最后一个位置

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
 
-    HashSet<List<Integer>> ans = new HashSet<>();
-    LinkedList<Integer> list = new LinkedList<>();
-    HashSet<Integer> set = new HashSet<>();
-    int[] nums = new int[100];
-    ArrayList<Integer> numList;
-
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        for (int candidate : candidates) {
-            nums[candidate]++;
-            set.add(candidate);
-        }
-        numList = new ArrayList<>(set);
-
-        dfs(0, target, 0);
-        return new ArrayList<>(ans);
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{find(nums, target, true), find(nums, target, false)};
     }
 
-    private void dfs(int i, int target, int sum) {
-        if (sum == target) {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-
-        if (sum > target || i >= numList.size()) {
-            return;
-        }
-
-        int val = numList.get(i);
-
-        for (int j = 0; j <= nums[val]; j++) {
-            for (int k = 0; k < j; k++) {
-                list.add(val);
+    public int find(int[] nums, int target, boolean isLower) {
+        int left = 0, right = nums.length - 1, mid;
+        int ans = -1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
             }
-
-            dfs(i + 1, target, sum + val * j);
-
-            for (int k = 0; k < j; k++) {
-                list.removeLast();
+            if (nums[mid] > target || nums[mid] == target && isLower) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+        return ans;
     }
-
 }
