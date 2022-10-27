a9f54cb125fa2e02461e5ddf5060d7fc032e97cb
yanglongqi <yanglongqi@xiaomi.com>
2022-10-28 00:57:35 +0800

    [41]缺失的第一个正数

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,24 @@
 class Solution {
 
-    public int[] searchRange(int[] nums, int target) {
-        return new int[]{find(nums, target, true), find(nums, target, false)};
-    }
-
-    public int find(int[] nums, int target, boolean lower) {
-        int left = 0;
-        int right = nums.length - 1;
-        int ans = -1;
-        int mid;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] <= nums.length) {
+                swap(nums, i, nums[i] - 1);
             }
-
-            if (nums[mid] > target || nums[mid] == target && lower) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-
         }
 
-        return ans;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
