c8178020e41ec28171ce50dc2f1668f0a355b596
yanglongqi <yanglongqi@xiaomi.com>
2022-10-30 00:26:33 +0800

    [44]通配符匹配

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,32 @@
-import java.util.ArrayList;
-
 class Solution {
 
-    public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) {
-            return "0";
+    public boolean isMatch(String s, String p) {
+        int m = p.length();
+        int n = s.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            if (p.charAt(i - 1) != '*') {
+                break;
+            }
+            dp[i][0] = true;
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        int t = 0;
-        int n = num1.length() - 1;
-        int m = num2.length() - 1;
-
-        for (int j = m; j >= 0; j--) {
-            for (int i = n; i >= 0; i--) {
-                int index = (n - i) + (m - j);
-                int num = (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + t;
-                if (index >= list.size()) {
-                    t = num / 10;
-                    list.add(num % 10);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                 } else {
-                    num += list.get(index);
-                    t = num / 10;
-                    list.set(index, num % 10);
+                    dp[i][j] = dp[i - 1][j - 1] && isMatch(p.charAt(i - 1), s.charAt(j - 1));
                 }
             }
-            if (t != 0) {
-                list.add(t);
-                t = 0;
-            }
-            // System.out.println(list.toString());
         }
-        StringBuilder sb = new StringBuilder();
-        for (Integer integer : list) {
-            sb.append(integer);
-        }
-        return sb.reverse().toString();
-
+        return dp[m][n];
     }
 
+    private boolean isMatch(char c1, char c2) {
+        if (c1 == '?') {
+            return true;
+        }
+        return c1 == c2;
+    }
 }
