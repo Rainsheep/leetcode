29dd8ae89f852ae6ca511c7d1e92eb2ccbdb6478
yanglongqi <yanglongqi@xiaomi.com>
2022-10-30 17:28:14 +0800

    [复习][44]通配符匹配

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,34 @@
-import java.util.ArrayList;
-
 class Solution {
 
-    public String multiply(String num1, String num2) {
-        int m = num2.length() - 1;
-        int n = num1.length() - 1;
-        ArrayList<Integer> list = new ArrayList<>();
-        int t = 0;
-        for (int i = m; i >= 0; i--) {
-            for (int j = n; j >= 0; j--) {
-                int index = m - i + n - j;
-                int old = index >= list.size() ? 0 : list.get(index);
-                int num = (num2.charAt(i) - '0') * (num1.charAt(j) - '0') + t + old;
-                t = num / 10;
-                if (index >= list.size()) {
-                    list.add(num % 10);
-                } else {
-                    list.set(index, num % 10);
-                }
+    public boolean isMatch(String s, String p) {
+        int m = p.length();
+        int n = s.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            if (p.charAt(i - 1) != '*') {
+                break;
             }
-            list.add(t);
-            t = 0;
+            dp[i][0] = true;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (Integer integer : list) {
-            sb.append(integer);
-        }
-        sb = sb.reverse();
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) != '0') {
-                return sb.substring(i);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && match(p.charAt(i - 1), s.charAt(j - 1));
+                }
             }
         }
-        return "0";
+        return dp[m][n];
     }
+
+    public boolean match(char c1, char c2) {
+        if (c1 == '?') {
+            return true;
+        }
+        return c1 == c2;
+    }
+
 }
