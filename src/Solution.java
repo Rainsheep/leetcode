824e4f2e48f39364101ec09c46b0f72f29689ad3
yanglongqi <yanglongqi@xiaomi.com>
2022-10-30 17:51:34 +0800

    [45]跳跃游戏 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,16 @@
 class Solution {
 
-    public boolean isMatch(String s, String p) {
-        int m = p.length();
-        int n = s.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            if (p.charAt(i - 1) != '*') {
-                break;
-            }
-            dp[i][0] = true;
-        }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p.charAt(i - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] && match(p.charAt(i - 1), s.charAt(j - 1));
-                }
+    public int jump(int[] nums) {
+        int step = 0;
+        int maxIndex = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxIndex = Math.max(maxIndex, nums[i] + i);
+            if (i == end) {
+                end = maxIndex;
+                step++;
             }
         }
-        return dp[m][n];
+        return step;
     }
-
-    public boolean match(char c1, char c2) {
-        if (c1 == '?') {
-            return true;
-        }
-        return c1 == c2;
-    }
-
 }
