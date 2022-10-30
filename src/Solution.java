0efa82cdf6a3b56cdc11c19dfd562cb282271545
yanglongqi <yanglongqi@xiaomi.com>
2022-10-30 17:01:52 +0800

    [复习][43]字符串相乘

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,38 @@
+import java.util.ArrayList;
+
 class Solution {
 
-    public int trap(int[] height) {
-        int n = height.length;
-        int ans = 0;
-        int leftMax = height[0];
-        int rightMax = height[height.length - 1];
-        int left = 1;
-        int right = height.length - 2;
-        while (left <= right) {
-            if (leftMax <= rightMax) {
-                leftMax = Math.max(leftMax, height[left]);
-                ans += leftMax - height[left++];
-            } else {
-                rightMax = Math.max(rightMax, height[right]);
-                ans += rightMax - height[right--];
+    public String multiply(String num1, String num2) {
+        int m = num2.length() - 1;
+        int n = num1.length() - 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        int t = 0;
+        for (int i = m; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                int index = m - i + n - j;
+                int old = index >= list.size() ? 0 : list.get(index);
+                int num = (num2.charAt(i) - '0') * (num1.charAt(j) - '0') + t + old;
+                t = num / 10;
+                if (index >= list.size()) {
+                    list.add(num % 10);
+                } else {
+                    list.set(index, num % 10);
+                }
+            }
+            list.add(t);
+            t = 0;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Integer integer : list) {
+            sb.append(integer);
+        }
+        sb = sb.reverse();
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) != '0') {
+                return sb.substring(i);
             }
         }
-        return ans;
-
+        return "0";
     }
 }
