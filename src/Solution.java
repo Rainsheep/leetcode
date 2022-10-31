2fd7163432fec31d8d24c9d1ae3e68657f089bf7
yanglongqi <yanglongqi@xiaomi.com>
2022-10-31 14:46:31 +0800

    [复习][45]跳跃游戏 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,20 @@
 class Solution {
 
-    public boolean isMatch(String s, String p) {
-        int m = p.length();
-        int n = s.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
+    public int jump(int[] nums) {
+        // 现在的步数
+        int step = 0;
+        // 这一步的最远距离
+        int end = 0;
+        // 下一步的最远距离
+        int maxIndex = 0;
 
-        for (int i = 1; i <= m; i++) {
-            if (p.charAt(i - 1) == '*') {
-                dp[i][0] = true;
-                continue;
-            }
-            break;
-        }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p.charAt(i - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] && match(p.charAt(i - 1), s.charAt(j - 1));
-                }
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+            if (i == end) {
+                end = maxIndex;
+                step++;
             }
         }
-
-        return dp[m][n];
+        return step;
     }
-
-    public boolean match(char c1, char c2) {
-        if (c1 == '?') {
-            return true;
-        }
-        return c1 == c2;
-    }
-
 }
