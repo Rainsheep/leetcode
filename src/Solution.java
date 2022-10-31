794bc2f9b5c9ac4b98e207da57ceee582ef71332
yanglongqi <yanglongqi@xiaomi.com>
2022-10-31 17:03:06 +0800

    [47]全排列 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -6,7 +7,7 @@ class Solution {
 
     List<List<Integer>> ans = new ArrayList<>();
 
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         dfs(new LinkedList<>(), nums, new boolean[nums.length]);
         return ans;
     }
@@ -16,9 +17,11 @@ class Solution {
             ans.add(new ArrayList<>(list));
             return;
         }
+        HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < flag.length; i++) {
-            if (!flag[i]) {
+            if (!flag[i] && !set.contains(nums[i])) {
+                set.add(nums[i]);
                 list.add(nums[i]);
                 flag[i] = true;
                 dfs(list, nums, flag);
