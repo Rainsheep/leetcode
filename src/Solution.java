10ee8f52c80dd9c9e0968ee8d55ee8d7d9aa713a
yanglongqi <yanglongqi@xiaomi.com>
2022-11-04 15:40:29 +0800

    [复习][24]两两交换链表中的节点

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        ArrayList<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < n - 2; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                if (left != i + 1 && nums[left] == nums[left - 1]) {
-                    left++;
-                    continue;
-                }
-                int res = nums[i] + nums[left] + nums[right];
-                if (res == 0) {
-                    ans.add(List.of(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                } else if (res > 0) {
-                    right--;
-                } else {
-                    left++;
-                }
-            }
-
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
-        return ans;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
     }
 }
