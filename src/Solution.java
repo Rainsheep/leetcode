0eb254e9cc0c99e52c326861d51b7667fa1db426
yanglongqi <yanglongqi@xiaomi.com>
2022-11-04 19:08:50 +0800

    [复习][48]旋转图像

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
 
-    public int jump(int[] nums) {
-        int step = 0;
-        int end = 0;
-        // next step end index
-        int max = nums[0];
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            max = Math.max(max, i + nums[i]);
-            if (i == end) {
-                end = max;
-                step++;
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix, i, j, j, i);
             }
-
         }
-        return step;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix, i, j, i, n - 1 - j);
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        new Solution().jump(new int[]{1, 1, 1, 1});
+    private void swap(int[][] matrix, int x1, int y1, int x2, int y2) {
+        int temp = matrix[x1][y1];
+        matrix[x1][y1] = matrix[x2][y2];
+        matrix[x2][y2] = temp;
     }
 }
