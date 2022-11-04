e395c90ababf87e4cdf70e16356f6c93a266950a
yanglongqi <yanglongqi@xiaomi.com>
2022-11-04 17:35:56 +0800

    [复习][34]在排序数组中查找元素的第一个和最后一个位置

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,35 @@
 class Solution {
 
-    public int search(int[] nums, int target) {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{find(nums, target, true), find(nums, target, false)};
+    }
+
+    private int find(int[] nums, int target, boolean lower) {
         int left = 0;
         int right = nums.length - 1;
+        int res = -1;
         int mid;
         while (left <= right) {
             mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                return mid;
+                res = mid;
+                if (lower) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+                continue;
             }
-            if (nums[mid] >= nums[right]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                left = mid + 1;
             }
         }
-        return -1;
+
+        return res;
+
 
     }
 }
