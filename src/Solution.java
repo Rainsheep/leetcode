ae5c235575a1921daef05c6ce34e319df39fa4ba
yanglongqi <yanglongqi@xiaomi.com>
2022-11-04 16:16:37 +0800

    [复习][29]两数相除

--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,45 @@
-
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import java.util.LinkedList;
 
 class Solution {
 
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null) {
-            return null;
-        }
-        ListNode end = head;
-        for (int i = 0; i < k; i++) {
-            end = end.next;
-            if (i != k - 1 && end == null) {
-                return head;
+    public int divide(int dividend, int divisor) {
+        boolean isNegative = (((dividend ^ divisor) >> 31) & 1) == 1;
+
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
             }
         }
-        ListNode newHead = reverse(head, end);
-        head.next = reverseKGroup(end, k);
-        return newHead;
-
-    }
-
-    public ListNode reverse(ListNode head, ListNode end) {
-        ListNode pre = null;
-        ListNode next;
-        ListNode cur = head;
-        while (cur != end) {
-            next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
+        if (dividend > 0) {
+            dividend = -dividend;
         }
-        return pre;
+        if (divisor > 0) {
+            divisor = -divisor;
+        }
+
+        if (dividend > divisor) {
+            return 0;
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(divisor);
+        int res = 1;
+        while (dividend - list.get(list.size() - 1) <= list.get(list.size() - 1)) {
+            list.add(list.get(list.size() - 1) + list.get(list.size() - 1));
+            res <<= 1;
+        }
+
+        int ans = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (dividend <= list.get(i)) {
+                dividend -= list.get(i);
+                ans += res;
+            }
+            res >>= 1;
+        }
+        return isNegative ? -ans : ans;
     }
 }
