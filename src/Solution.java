a384bbce5cc364d53fc4791b83d9e108589f266a
yanglongqi <yanglongqi@xiaomi.com>
2022-11-09 14:31:36 +0800

    [51]N 皇后

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
 
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                swap(matrix, i, j, j, i);
-            }
-        }
+    int n;
+    boolean[] rowFlag;
+    boolean[] colFlag;
+    boolean[] slashFlag;
+    boolean[] backslashFlag;
+    boolean[][] map;
+    int num;
+    List<List<String>> ans = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                swap(matrix, i, j, i, n - 1 - j);
-            }
-        }
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        rowFlag = new boolean[n];
+        colFlag = new boolean[n];
+        slashFlag = new boolean[2 * n];
+        backslashFlag = new boolean[2 * n];
+        map = new boolean[n][n];
+
+        dfs(0, 0);
+        return ans;
     }
 
-    private void swap(int[][] matrix, int x1, int y1, int x2, int y2) {
-        int temp = matrix[x1][y1];
-        matrix[x1][y1] = matrix[x2][y2];
-        matrix[x2][y2] = temp;
+    public void dfs(int x, int y) {
+        if (x == n) {
+            if (num == n) {
+                ans.add(genAnsList());
+            }
+            return;
+        }
+
+        int[] nextIndex = nextIndex(x, y);
+        boolean res = check(x, y);
+        if (res) {
+            put(x, y);
+            dfs(nextIndex[0], nextIndex[1]);
+            remove(x, y);
+        }
+
+        dfs(nextIndex[0], nextIndex[1]);
+    }
+
+    private List<String> genAnsList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb.append(map[i][j] ? 'Q' : '.');
+            }
+            list.add(sb.toString());
+        }
+        return list;
+    }
+
+    private void remove(int x, int y) {
+        rowFlag[x] = false;
+        colFlag[y] = false;
+        slashFlag[x + y] = false;
+        backslashFlag[x - y + n] = false;
+        num--;
+        map[x][y] = false;
+    }
+
+    private void put(int x, int y) {
+        rowFlag[x] = true;
+        colFlag[y] = true;
+        slashFlag[x + y] = true;
+        backslashFlag[x - y + n] = true;
+        num++;
+        map[x][y] = true;
+    }
+
+    private boolean check(int x, int y) {
+        if (rowFlag[x]) {
+            return false;
+        }
+        if (colFlag[y]) {
+            return false;
+        }
+        if (slashFlag[x + y]) {
+            return false;
+        }
+        if (backslashFlag[x - y + n]) {
+            return false;
+        }
+        return true;
+    }
+
+    public int[] nextIndex(int x, int y) {
+        if (y == n - 1) {
+            return new int[]{x + 1, 0};
+        }
+        return new int[]{x, y + 1};
     }
 }
