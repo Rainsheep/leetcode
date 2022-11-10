3cbf02bb9ca9c4791651bd46ad6d2812d938fa11
yanglongqi <yanglongqi@xiaomi.com>
2022-11-10 13:15:46 +0800

    [复习][52]N皇后 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,77 +1,23 @@
 class Solution {
 
-    int n;
-    boolean[] rowFlag;
-    boolean[] colFlag;
-    boolean[] slashFlag;
-    boolean[] backslashFlag;
-    int num;
-    int ans;
-
     public int totalNQueens(int n) {
-        this.n = n;
-        rowFlag = new boolean[n];
-        colFlag = new boolean[n];
-        slashFlag = new boolean[2 * n];
-        backslashFlag = new boolean[2 * n];
+        return dfs(n, 0, 0, 0, 0);
+    }
 
-        dfs(0, 0);
+    private int dfs(int n, int row, int colFlag, int slashFlag, int backSlashFlag) {
+        if (row == n) {
+            return 1;
+        }
+        int ans = 0;
+        // 1 为可用
+        int usablePosition = ((1 << n) - 1) & ~(colFlag | slashFlag | backSlashFlag);
+        while (usablePosition != 0) {
+            int position = usablePosition & -usablePosition;
+            usablePosition = usablePosition & (usablePosition - 1);
+            ans += dfs(n, row + 1, colFlag | position, (slashFlag | position) >> 1, (backSlashFlag | position) << 1);
+        }
         return ans;
     }
 
-    public void dfs(int x, int y) {
-        if (x == n) {
-            ans += num == n ? 1 : 0;
-            return;
-        }
 
-        int[] nextIndex = nextIndex(x, y);
-        boolean res = check(x, y);
-        if (res) {
-            put(x, y);
-            dfs(nextIndex[0], nextIndex[1]);
-            remove(x, y);
-        }
-
-        dfs(nextIndex[0], nextIndex[1]);
-    }
-
-    private void remove(int x, int y) {
-        rowFlag[x] = false;
-        colFlag[y] = false;
-        slashFlag[x + y] = false;
-        backslashFlag[x - y + n] = false;
-        num--;
-    }
-
-    private void put(int x, int y) {
-        rowFlag[x] = true;
-        colFlag[y] = true;
-        slashFlag[x + y] = true;
-        backslashFlag[x - y + n] = true;
-        num++;
-    }
-
-    private boolean check(int x, int y) {
-        if (rowFlag[x]) {
-            return false;
-        }
-        if (colFlag[y]) {
-            return false;
-        }
-        if (slashFlag[x + y]) {
-            return false;
-        }
-        if (backslashFlag[x - y + n]) {
-            return false;
-        }
-        return true;
-    }
-
-    public int[] nextIndex(int x, int y) {
-        if (y == n - 1) {
-            return new int[]{x + 1, 0};
-        }
-        return new int[]{x, y + 1};
-    }
 }
