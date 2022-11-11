d41f5cb2c781ab9690f5e2b24251f008a5806931
yanglongqi <yanglongqi@xiaomi.com>
2022-11-11 14:47:10 +0800

    [54]螺旋矩阵

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
 
-    public int maxSubArray(int[] nums) {
-        int maxElem = nums[0];
-        int max = 0;
-        int sum = 0;
-        for (int num : nums) {
-            maxElem = Math.max(num, maxElem);
-            if (sum < 0) {
-                sum = 0;
-            }
-            sum += num;
-            max = Math.max(max, sum);
+    int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int k = 0;
+    boolean[][] flag;
+    int m, n;
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        m = matrix.length;
+        n = matrix[0].length;
+        flag = new boolean[m][n];
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int x = 0, y = 0;
+        flag[x][y] = true;
+        ans.add(matrix[x][y]);
+        while (ans.size() != m * n) {
+            int[] nextIndex = nextIndex(x, y);
+            x = nextIndex[0];
+            y = nextIndex[1];
+            ans.add(matrix[x][y]);
+            flag[x][y] = true;
         }
 
-        return maxElem >= 0 ? max : maxElem;
+        return ans;
+
+    }
+
+    private int[] nextIndex(int x, int y) {
+        int newX = x + dir[k][0];
+        int newY = y + dir[k][1];
+        if (!check(newX, newY)) {
+            k = (k + 1) % 4;
+            newX = x + dir[k][0];
+            newY = y + dir[k][1];
+        }
+        return new int[]{newX, newY};
+    }
+
+    private boolean check(int x, int y) {
+        return x < m && y < n && x >= 0 && y >= 0 && !flag[x][y];
     }
 }
