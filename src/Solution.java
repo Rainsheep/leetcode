8d8557903a777a034f9bf4adb9123516213459b2
yanglongqi <yanglongqi@xiaomi.com>
2022-11-14 12:48:44 +0800

    [复习][52]N皇后 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,23 @@
 class Solution {
 
-    public boolean canJump(int[] nums) {
-        int maxIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (maxIndex < i) {
-                return false;
-            }
-            maxIndex = Math.max(maxIndex, i + nums[i]);
-        }
-        return true;
+    public int totalNQueens(int n) {
+        return dfs(n, 0, 0, 0, 0);
     }
+
+    private int dfs(int n, int row, int colFlag, int slashFlag, int backSlashFlag) {
+        if (row == n) {
+            return 1;
+        }
+        int usablePosition = (1 << n) - 1 & ~(colFlag | slashFlag | backSlashFlag);
+        int ans = 0;
+        while (usablePosition != 0) {
+            int position = usablePosition & -usablePosition;
+            ans += dfs(n, row + 1, colFlag | position, (slashFlag | position) >> 1, (backSlashFlag | position) << 1);
+            usablePosition &= usablePosition - 1;
+        }
+
+        return ans;
+    }
+
+
 }
