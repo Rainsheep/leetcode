7f9b85d7142a3f572f0466bf0c7d9463d9b5e322
yanglongqi <yanglongqi@xiaomi.com>
2022-11-17 19:42:26 +0800

    [59]螺旋矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,37 @@
 class Solution {
 
-    public int lengthOfLastWord(String s) {
-        int ans = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                if (ans == 0) {
-                    continue;
-                }
-                break;
+    int[][] ans;
+    int n;
+    int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int k = 0;
+
+    public int[][] generateMatrix(int n) {
+        this.n = n;
+        ans = new int[n][n];
+        int[] nowIndex = {0, 0};
+        for (int i = 0; i < n * n; i++) {
+            if (i != 0) {
+                nowIndex = nextIndex(nowIndex[0], nowIndex[1]);
             }
-            ans++;
+
+            ans[nowIndex[0]][nowIndex[1]] = i + 1;
         }
         return ans;
+
     }
+
+    private int[] nextIndex(int x, int y) {
+        int nextX = x + dir[k][0];
+        int nextY = y + dir[k][1];
+        if (checkIndex(nextX, nextY)) {
+            return new int[]{nextX, nextY};
+        }
+        k = (k + 1) % 4;
+        return new int[]{x + dir[k][0], y + dir[k][1]};
+    }
+
+    private boolean checkIndex(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n && ans[x][y] == 0;
+    }
+
 }
