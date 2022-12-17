945b1b3b4e78012f897d3b8c880658a554a15935
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 12:20:43 +0800

    [5]最长回文子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,45 +1,43 @@
 class Solution {
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len = nums1.length + nums2.length;
-        if (len % 2 != 0) {
-            return findIndex(nums1, 0, nums2, 0, len / 2 + 1);
-        } else {
-            int val1 = findIndex(nums1, 0, nums2, 0, len / 2);
-            int val2 = findIndex(nums1, 0, nums2, 0, len / 2 + 1);
-            return (val1 + val2) / 2.0;
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            String ret = fun(s, i);
+            if (ret.length() > res.length()) {
+                res = ret;
+            }
         }
+        return res;
 
     }
 
-    public int findIndex(int[] num1, int num1BeginIndex, int[] num2, int num2BeginIndex, int k) {
-        if (num1BeginIndex == num1.length) {
-            return num2[num2BeginIndex + k - 1];
+    public String fun(String s, int index) {
+        int len = 1;
+        int left = index - 1;
+        int right = index + 1;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            len++;
         }
-        if (num2BeginIndex == num2.length) {
-            return num1[num1BeginIndex + k - 1];
-        }
+        String ret1 = s.substring(index - len + 1, index + len);
 
-        if (k == 1) {
-            return Math.min(num1[num1BeginIndex], num2[num2BeginIndex]);
+        len = 0;
+        left = index;
+        right = index + 1;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            len++;
         }
+        String ret2 = s.substring(index - len + 1, index + len + 1);
+        return ret1.length() >= ret2.length() ? ret1 : ret2;
 
-        int index1 = Math.min(num1BeginIndex + k / 2 - 1, num1.length - 1);
-        int index2 = Math.min(num2BeginIndex + k / 2 - 1, num2.length - 1);
-        if (num1[index1] <= num2[index2]) {
-            k -= index1 + 1 - num1BeginIndex;
-            num1BeginIndex = index1 + 1;
-        } else {
-            k -= index2 + 1 - num2BeginIndex;
-            num2BeginIndex = index2 + 1;
-        }
-        return findIndex(num1, num1BeginIndex, num2, num2BeginIndex, k);
     }
-
 
     public static void main(String[] args) {
-        int[] num1 = new int[]{1, 2, 3};
-        int[] num2 = new int[]{4, 5, 6};
-        System.out.println(new Solution().findIndex(num1, 0, num2, 0, 4));
+        System.out.println(new Solution().longestPalindrome("cbbd"));
     }
 }
