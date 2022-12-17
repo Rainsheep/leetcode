6528bd830478af440411f1a764a93d274be1dfb3
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 14:35:35 +0800

    [12]整数转罗马数字

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,82 @@
 class Solution {
 
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int ret = 0;
-
-        while (left < right) {
-            ret = Math.max(ret, (right - left) * Math.min(height[left], height[right]));
-            if (height[left] <= height[right]) {
-                left++;
-            } else {
-                right--;
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            if (num >= 1000) {
+                num -= 1000;
+                sb.append('M');
+                continue;
             }
+            if (num >= 900) {
+                num -= 900;
+                sb.append("CM");
+                continue;
+            }
+            if (num >= 500) {
+                num -= 500;
+                sb.append('D');
+                continue;
+            }
+
+            if (num >= 400) {
+                num -= 400;
+                sb.append("CD");
+                continue;
+            }
+
+            if (num >= 100) {
+                num -= 100;
+                sb.append('C');
+                continue;
+            }
+
+            if (num >= 90) {
+                num -= 90;
+                sb.append("XC");
+                continue;
+            }
+
+            if (num >= 50) {
+                num -= 50;
+                sb.append('L');
+                continue;
+            }
+
+            if (num >= 40) {
+                num -= 40;
+                sb.append("XL");
+                continue;
+            }
+
+            if (num >= 10) {
+                num -= 10;
+                sb.append('X');
+                continue;
+            }
+
+            if (num >= 9) {
+                num -= 9;
+                sb.append("IX");
+                continue;
+            }
+
+            if (num >= 5) {
+                num -= 5;
+                sb.append('V');
+                continue;
+            }
+
+            if (num == 4) {
+                num -= 4;
+                sb.append("IV");
+                continue;
+            }
+
+            num -= 1;
+            sb.append('I');
         }
 
-        return ret;
-
+        return sb.toString();
     }
 }
