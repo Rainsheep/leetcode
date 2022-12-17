b32108248c731d04836d61955a6d607dd6e32e9a
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 14:35:06 +0800

    [11]盛最多水的容器

--- src/Solution.java
+++ src/Solution.java
@@ -1,44 +1,20 @@
 class Solution {
 
-    public boolean isMatch(String s, String p) {
-        int sLen = s.length();
-        int pLen = p.length();
-        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
-        dp[0][0] = true;
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int ret = 0;
 
-        for (int i = 1; i < pLen; i += 2) {
-            if (p.charAt(i) == '*') {
-                dp[0][i + 1] = true;
+        while (left < right) {
+            ret = Math.max(ret, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] <= height[right]) {
+                left++;
             } else {
-                break;
+                right--;
             }
         }
 
-        for (int i = 1; i <= sLen; i++) {
-            for (int j = 1; j <= pLen; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 2];
-                    if (isMatch(s.charAt(i - 1), p.charAt(j - 2))) {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
+        return ret;
 
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] && isMatch(s.charAt(i - 1), p.charAt(j - 1));
-                }
-            }
-        }
-        return dp[sLen][pLen];
-    }
-
-
-    private boolean isMatch(char sc, char pc) {
-        if (pc == '.') {
-            return true;
-        }
-        return sc == pc;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Solution().isMatch("a", "a*"));
     }
 }
