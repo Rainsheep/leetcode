deb65af0fc20031b940abfa5ef42b249e384febb
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 11:07:49 +0800

    [2]两数相加

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,46 @@
-import java.util.HashMap;
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
 
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int t = 0;
+        ListNode head = new ListNode();
+        ListNode p = head;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int num = num1 + num2 + t;
+            t = num / 10;
+            p.next = new ListNode(num % 10);
+            p = p.next;
+            if (l1 != null) {
+                l1 = l1.next;
             }
-            map.put(nums[i], i);
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
-        return new int[0];
+        if (t != 0) {
+            p.next = new ListNode(t);
+        }
+
+        return head.next;
+
     }
 }
