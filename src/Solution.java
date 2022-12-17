32066e5c26bcd8eaf010621aea35fe6879fe80f6
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 15:35:06 +0800

    [19]删除链表的倒数第 N 个结点

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,46 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
 
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums.length < 4) {
-            return ans;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode now = head;
+
+        ListNode pre = new ListNode();
+        pre.next = now;
+
+        ListNode p = head;
+
+        int k = n + 1;
+        while (k-- > 1) {
+            p = p.next;
         }
 
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 3; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-
-                int left = j + 1;
-                int right = nums.length - 1;
-
-                while (left < right) {
-                    if (left != j + 1 && nums[left] == nums[left - 1]) {
-                        left++;
-                        continue;
-                    }
-
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left++;
-                        right--;
-                    } else if (sum > target) {
-                        right--;
-                    } else {
-                        left++;
-                    }
-                }
-
-            }
+        while (p != null) {
+            p = p.next;
+            now = now.next;
+            pre = pre.next;
         }
 
-        return ans;
-
+        if (pre.next == head) {
+            return head.next;
+        }
+        pre.next = pre.next.next;
+        return head;
     }
-
 }
