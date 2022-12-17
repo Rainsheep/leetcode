69b8419dce76064aa53b290fcfd1d3a5e3f5db49
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 14:30:30 +0800

    [10]正则表达式匹配

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,44 @@
 class Solution {
 
-    public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+
+        for (int i = 1; i < pLen; i += 2) {
+            if (p.charAt(i) == '*') {
+                dp[0][i + 1] = true;
+            } else {
+                break;
+            }
         }
-        if (x == 0) {
-            return true;
+
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (isMatch(s.charAt(i - 1), p.charAt(j - 2))) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && isMatch(s.charAt(i - 1), p.charAt(j - 1));
+                }
+            }
         }
-        if (x % 10 == 0) {
-            return false;
-        }
-        return x == reverse(x);
+        return dp[sLen][pLen];
     }
 
-    private long reverse(long x) {
-        long ret = 0;
-        while (x != 0) {
-            ret = ret * 10 + x % 10;
-            x /= 10;
+
+    private boolean isMatch(char sc, char pc) {
+        if (pc == '.') {
+            return true;
         }
-        return ret;
+        return sc == pc;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isMatch("a", "a*"));
     }
 }
