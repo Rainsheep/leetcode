114f86ce2d6f83cf33746ad3f7b5d9f78cef0f67
yanglongqi <yanglongqi@xiaomi.com>
2022-12-17 14:09:47 +0800

    [9]回文数

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,24 @@
 class Solution {
 
-    public int myAtoi(String s) {
-        s = s.trim();
-        int flag = 1;
-        if (s.length() == 0) {
-            return 0;
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
-
-        if (s.charAt(0) == '-') {
-            flag = -1;
-            s = s.substring(1);
-        } else if (s.charAt(0) == '+') {
-            s = s.substring(1);
+        if (x == 0) {
+            return true;
         }
-
-        long res = 0;
-        int index = 0;
-        while (index < s.length() && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
-            res = res * 10 + s.charAt(index) - '0';
-            index++;
-
-            if (res * flag > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            }
-            if (res * flag < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            }
+        if (x % 10 == 0) {
+            return false;
         }
+        return x == reverse(x);
+    }
 
-        res = res * flag;
-
-        return (int) res;
+    private long reverse(long x) {
+        long ret = 0;
+        while (x != 0) {
+            ret = ret * 10 + x % 10;
+            x /= 10;
+        }
+        return ret;
     }
 }
