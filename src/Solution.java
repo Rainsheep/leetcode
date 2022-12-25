529c2c92ef50ebba0fd4abe2693d6a99dd0003c2
yanglongqi <yanglongqi@xiaomi.com>
2022-12-25 16:52:36 +0800

    [复习][45]跳跃游戏 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,19 @@
-import java.util.ArrayList;
-
 class Solution {
 
-    public String multiply(String num1, String num2) {
-        int m = num2.length() - 1;
-        int n = num1.length() - 1;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = n; i >= 0; i--) {
-            for (int j = m; j >= 0; j--) {
-                int index = n - i + m - j;
+    public int jump(int[] nums) {
+        int nowMax = 0;
+        int nextMax = nums[0];
+        int step = 0;
 
-                int num = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                if (index < list.size()) {
-                    list.set(index, list.get(index) + num);
-                } else {
-                    list.add(num);
-                }
+        for (int i = 0; i < nums.length - 1; i++) {
+            nextMax = Math.max(nextMax, i + nums[i]);
+            if (i == nowMax) {
+                step++;
+                nowMax = nextMax;
             }
+
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            int num = list.get(i);
-            if (i + 1 < list.size()) {
-                list.set(i + 1, list.get(i + 1) + num / 10);
-            } else if (num / 10 > 0) {
-                list.add(num / 10);
-            }
-            list.set(i, num % 10);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = list.size() - 1; i >= 0; i--) {
-            sb.append(list.get(i));
-        }
-
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) != '0') {
-                return sb.substring(i);
-            }
-        }
-
-        return "0";
-    }
-
-    public static void main(String[] args) {
-        new Solution().multiply("123", "456");
+        return step;
     }
 }
