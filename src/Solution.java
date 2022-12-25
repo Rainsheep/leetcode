61c16fb47799319b890a3991e90392554a66cf85
yanglongqi <yanglongqi@xiaomi.com>
2022-12-25 18:55:00 +0800

    [复习][33]搜索旋转排序数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public void nextPermutation(int[] nums) {
-        int k;
-        for (k = nums.length - 1; k > 0; k--) {
-            if (nums[k] > nums[k - 1]) {
-                break;
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+                if (nums[left] > nums[right] && nums[mid] >= nums[left] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            } else {
+                if (nums[left] > nums[right] && nums[mid] < nums[left] && target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
             }
         }
+        return -1;
 
-        if (k == 0) {
-            Arrays.sort(nums);
-            return;
-        }
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] > nums[k - 1]) {
-                swap(nums, i, k - 1);
-                break;
-            }
-        }
-        Arrays.sort(nums, k, nums.length);
-
-    }
-
-    private void swap(int[] nums, int x, int y) {
-        int t = nums[x];
-        nums[x] = nums[y];
-        nums[y] = t;
     }
 }
