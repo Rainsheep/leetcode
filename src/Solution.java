e41201bfdf6b8afa50a07c3a970cc02a8f180cdf
yanglongqi <yanglongqi@xiaomi.com>
2022-12-25 12:16:45 +0800

    [复习][31]下一个排列

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,33 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
 
-    public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
-        boolean isNegative = (((dividend ^ divisor) >> 31) & 1) == 1;
-        if (dividend > 0) {
-            dividend = -dividend;
-        }
-        if (divisor > 0) {
-            divisor = -divisor;
-        }
-
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(divisor);
-        int num = 1;
-        while (dividend - divisor <= divisor) {
-            list.add(divisor << 1);
-            num <<= 1;
-            divisor <<= 1;
-        }
-
-        int ans = 0;
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (dividend <= list.get(i)) {
-                dividend -= list.get(i);
-                ans += num;
+    public void nextPermutation(int[] nums) {
+        int k = nums.length - 1;
+        for (; k > 0; k--) {
+            if (nums[k] > nums[k - 1]) {
+                break;
             }
-            num >>= 1;
         }
-        return isNegative ? -ans : ans;
+
+        if (k == 0) {
+            Arrays.sort(nums);
+            return;
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > nums[k - 1]) {
+                swap(nums, i, k - 1);
+                break;
+            }
+        }
+
+        Arrays.sort(nums, k, nums.length);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
