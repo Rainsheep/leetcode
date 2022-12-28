d5abc6ef1667af038d30fa0d47215af6af4de3bc
yanglongqi <yanglongqi@xiaomi.com>
2022-12-28 12:45:46 +0800

    [65]有效数字

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,66 @@
 class Solution {
 
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int min;
-                if (i == 0 && j == 0) {
-                    min = 0;
-                } else if (i == 0) {
-                    min = dp[i][j - 1];
-                } else if (j == 0) {
-                    min = dp[i - 1][j];
-                } else {
-                    min = Math.min(dp[i - 1][j], dp[i][j - 1]);
+    public boolean isNumber(String s) {
+        if (isFloat(s) || isInt(s)) {
+            return true;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'e' || c == 'E') {
+                String s1 = s.substring(0, i);
+                String s2 = s.substring(i + 1);
+                if ((isFloat(s1) || isInt(s1)) && isInt(s2)) {
+                    return true;
                 }
-                dp[i][j] = min + grid[i][j];
+
+            }
+        }
+        return false;
+    }
+
+    private boolean isFloat(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+        int k = 0;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            k++;
+        }
+        if (k == s.length()) {
+            return false;
+        }
+
+        int intNum = 0;
+        int pointNum = 0;
+        for (; k < s.length(); k++) {
+            if (s.charAt(k) == '.') {
+                pointNum++;
+            } else if (s.charAt(k) >= '0' && s.charAt(k) <= '9') {
+                intNum++;
+            } else {
+                return false;
             }
         }
 
-        return dp[m - 1][n - 1];
+        return intNum > 0 && pointNum == 1;
+    }
 
+    private boolean isInt(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+        int k = 0;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            k++;
+        }
+        if (k == s.length()) {
+            return false;
+        }
+        for (; k < s.length(); k++) {
+            if (s.charAt(k) < '0' || s.charAt(k) > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 }
