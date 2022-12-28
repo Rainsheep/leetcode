c22817d1b55eb2f05504eb8a1f912aa2d214c4d0
yanglongqi <yanglongqi@xiaomi.com>
2022-12-28 09:47:52 +0800

    [64]最小路径和

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,27 @@
 class Solution {
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
         int[][] dp = new int[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dp[i][j] = getCount(i, j, m, n, obstacleGrid, dp);
+                int min;
+                if (i == 0 && j == 0) {
+                    min = 0;
+                } else if (i == 0) {
+                    min = dp[i][j - 1];
+                } else if (j == 0) {
+                    min = dp[i - 1][j];
+                } else {
+                    min = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+                dp[i][j] = min + grid[i][j];
             }
         }
-        return dp[m - 1][n - 1];
-    }
 
-    private int getCount(int x, int y, int m, int n, int[][] obstacleGrid, int[][] dp) {
-        if (x < 0 || x >= m || y < 0 || y >= n || obstacleGrid[x][y] == 1) {
-            return 0;
-        }
-        if (x == 0 && y == 0) {
-            return 1;
-        }
-        if (x == 0) {
-            return dp[x][y - 1];
-        }
-        if (y == 0) {
-            return dp[x - 1][y];
-        }
-        return dp[x - 1][y] + dp[x][y - 1];
+        return dp[m - 1][n - 1];
+
     }
 }
