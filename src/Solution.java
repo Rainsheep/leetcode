7f7c1f85e8b38c3e0e50707a1cf0ae78a70d5826
yanglongqi <yanglongqi@xiaomi.com>
2023-01-03 13:51:32 +0800

    [71]简化路径

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,35 @@
+import java.util.LinkedList;
+
 class Solution {
 
-    public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+    public String simplifyPath(String path) {
+        path = path.replaceAll("/+", "/");
+        String[] splits = path.split("/", 0);
+        LinkedList<String> stack = new LinkedList<>();
+        for (String split : splits) {
+            if ("".equals(split) || ".".equals(split)) {
+                continue;
+            }
+            if ("..".equals(split)) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+                continue;
+            }
+            stack.add(split);
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+
+        if (stack.isEmpty()) {
+            return "/";
         }
-        return dp[n];
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : stack) {
+            sb.append('/');
+            sb.append(s);
+        }
+
+        return sb.toString();
 
     }
 }
