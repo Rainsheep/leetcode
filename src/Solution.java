f53cca928897ae59f857c688b99bdc0c371e9b9c
yanglongqi <yanglongqi@xiaomi.com>
2023-01-03 12:43:55 +0800

    [70]爬楼梯

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,16 @@
 class Solution {
 
-    public int mySqrt(int x) {
-        int left = 0;
-        int right = x;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long t = mid * mid;
-            if (t == x) {
-                return (int) mid;
-            } else if (t > x) {
-                right = (int) (mid - 1);
-            } else {
-                left = (int) (mid + 1);
-            }
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
         }
-        return right;
-    }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
 
+    }
 }
