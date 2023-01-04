6a99434d192ea364bfeb50f56a1120bf9f362346
yanglongqi <yanglongqi@xiaomi.com>
2023-01-04 13:49:25 +0800

    [72]编辑距离

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,28 @@
-import java.util.LinkedList;
-
 class Solution {
 
-    public String simplifyPath(String path) {
-        path = path.replaceAll("/+", "/");
-        String[] splits = path.split("/", 0);
-        LinkedList<String> stack = new LinkedList<>();
-        for (String split : splits) {
-            if ("".equals(split) || ".".equals(split)) {
-                continue;
-            }
-            if ("..".equals(split)) {
-                if (!stack.isEmpty()) {
-                    stack.pollLast();
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < n + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                int left = dp[i - 1][j] + 1;
+                int right = dp[i][j - 1] + 1;
+                int leftDown = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    leftDown += 1;
                 }
-                continue;
+                dp[i][j] = Math.min(leftDown, Math.min(left, right));
             }
-            stack.add(split);
         }
 
-        if (stack.isEmpty()) {
-            return "/";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (String s : stack) {
-            sb.append('/');
-            sb.append(s);
-        }
-
-        return sb.toString();
-
+        return dp[m][n];
     }
 }
