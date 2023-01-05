7d3eb98f9f97cab6627a6b2abbf438817898b4ee
yanglongqi <yanglongqi@xiaomi.com>
2023-01-05 18:57:17 +0800

    [复习][76]最小覆盖子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
 
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean firstCol0 = false;
-        boolean firstRow0 = false;
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> flagMap = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int sLen = s.length();
 
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstCol0 = true;
-                break;
-            }
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            flagMap.put(c, flagMap.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                firstRow0 = true;
-                break;
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = matrix[0][j] = 0;
+        int left = 0;
+        int right = -1;
+        int lAns = 0;
+        int rAns = 0;
+        while (true) {
+            if (check(flagMap, map)) {
+                if (rAns == 0 || right - left + 1 < rAns - lAns) {
+                    lAns = left;
+                    rAns = right + 1;
                 }
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = 0;
+                char c = s.charAt(left);
+                map.put(c, map.get(c) - 1);
+                left++;
+            } else {
+                right++;
+                if (right == sLen) {
+                    break;
                 }
+                char c = s.charAt(right);
+                map.put(c, map.getOrDefault(c, 0) + 1);
             }
         }
+        return s.substring(lAns, rAns);
 
-        for (int i = 1; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 0; j < m; j++) {
-                    matrix[j][i] = 0;
-                }
+    }
+
+    private boolean check(Map<Character, Integer> flagMap, Map<Character, Integer> map) {
+        for (Character c : flagMap.keySet()) {
+            if (map.getOrDefault(c, 0) < flagMap.get(c)) {
+                return false;
             }
         }
-
-        if (firstRow0) {
-            for (int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-
-        if (firstCol0) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-
+        return true;
     }
 }
