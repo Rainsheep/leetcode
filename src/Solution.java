75af56771deb50e43bc9f19b9925052a10001046
yanglongqi <yanglongqi@xiaomi.com>
2023-01-08 23:06:59 +0800

    [77]组合

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,29 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
 
-    public String minWindow(String s, String t) {
-        HashMap<Character, Integer> flagMap = new HashMap<>();
-        HashMap<Character, Integer> map = new HashMap<>();
-        int sLen = s.length();
+    ArrayList<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            flagMap.put(c, flagMap.getOrDefault(c, 0) + 1);
-        }
-
-        int left = 0;
-        int right = -1;
-        int lAns = 0;
-        int rAns = 0;
-        while (true) {
-            if (check(flagMap, map)) {
-                if (rAns == 0 || right - left + 1 < rAns - lAns) {
-                    lAns = left;
-                    rAns = right + 1;
-                }
-                char c = s.charAt(left);
-                map.put(c, map.get(c) - 1);
-                left++;
-            } else {
-                right++;
-                if (right == sLen) {
-                    break;
-                }
-                char c = s.charAt(right);
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-        }
-        return s.substring(lAns, rAns);
-
+    public List<List<Integer>> combine(int n, int k) {
+        LinkedList<Integer> list = new LinkedList<>();
+        dfs(1, k, n, list);
+        return ans;
     }
 
-    private boolean check(Map<Character, Integer> flagMap, Map<Character, Integer> map) {
-        for (Character c : flagMap.keySet()) {
-            if (map.getOrDefault(c, 0) < flagMap.get(c)) {
-                return false;
-            }
+    private void dfs(int now, int k, int n, LinkedList<Integer> list) {
+        if (list.size() == k) {
+            ans.add(new ArrayList<>(list));
+            return;
         }
-        return true;
+        if (now == n + 1) {
+            return;
+        }
+
+        list.add(now);
+        dfs(now + 1, k, n, list);
+        list.removeLast();
+        dfs(now + 1, k, n, list);
     }
 }
