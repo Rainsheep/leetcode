cfba0350663e9308df7158b04bc6b252de26436f
yanglongqi <yanglongqi@xiaomi.com>
2023-01-08 23:33:04 +0800

    [79]单词搜索

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,56 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
 
-    ArrayList<List<Integer>> ans = new ArrayList<>();
+    int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    boolean ans = false;
 
-    public List<List<Integer>> subsets(int[] nums) {
-        LinkedList<Integer> list = new LinkedList<>();
-        dfs(0, nums, list);
-        return ans;
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] flag;
+        int len = word.length();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                flag = new boolean[m][n];
+                dfs(board, flag, word, i, j, 0);
+                if (ans) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    private void dfs(int index, int[] nums, LinkedList<Integer> list) {
-        if (index == nums.length) {
-            ans.add(new ArrayList<>(list));
+    private void dfs(char[][] board, boolean[][] flag, String word, int x, int y, int index) {
+        if (ans) {
             return;
         }
 
-        dfs(index + 1, nums, list);
+        if (index == word.length()) {
+            ans = true;
+            return;
+        }
 
-        list.add(nums[index]);
-        dfs(index + 1, nums, list);
-        list.removeLast();
+        if (!check(board, flag, word, x, y, index)) {
+            return;
+        }
+
+        flag[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            dfs(board, flag, word, x + dir[i][0], y + dir[i][1], index + 1);
+        }
+        flag[x][y] = false;
     }
+
+    private boolean check(char[][] board, boolean[][] flag, String word, int x, int y, int index) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if (x < 0 || x >= m || y < 0 || y >= n || flag[x][y]) {
+            return false;
+        }
+
+        return board[x][y] == word.charAt(index);
+    }
+
 }
