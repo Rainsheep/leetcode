7a8fd7dc6b2da5fe3c8107407e3d2b9b1272f1e0
yanglongqi <yanglongqi@xiaomi.com>
2023-01-08 23:39:42 +0800

    [80]删除有序数组中的重复项 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,56 +1,19 @@
 class Solution {
 
-    int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    boolean ans = false;
-
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] flag;
-        int len = word.length();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                flag = new boolean[m][n];
-                dfs(board, flag, word, i, j, 0);
-                if (ans) {
-                    return true;
-                }
+    public int removeDuplicates(int[] nums) {
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (len < 2) {
+                nums[len++] = nums[i];
+                continue;
             }
+
+            if (nums[i] == nums[len - 2]) {
+                continue;
+            }
+            nums[len++] = nums[i];
         }
-        return false;
+        return len;
+
     }
-
-    private void dfs(char[][] board, boolean[][] flag, String word, int x, int y, int index) {
-        if (ans) {
-            return;
-        }
-
-        if (index == word.length()) {
-            ans = true;
-            return;
-        }
-
-        if (!check(board, flag, word, x, y, index)) {
-            return;
-        }
-
-        flag[x][y] = true;
-        for (int i = 0; i < 4; i++) {
-            dfs(board, flag, word, x + dir[i][0], y + dir[i][1], index + 1);
-        }
-        flag[x][y] = false;
-    }
-
-    private boolean check(char[][] board, boolean[][] flag, String word, int x, int y, int index) {
-        int m = board.length;
-        int n = board[0].length;
-
-        if (x < 0 || x >= m || y < 0 || y >= n || flag[x][y]) {
-            return false;
-        }
-
-        return board[x][y] == word.charAt(index);
-    }
-
 }
