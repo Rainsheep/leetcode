185c48b3be84871d8e7a9fa847087ad637747acc
yanglongqi <yanglongqi@xiaomi.com>
2023-01-09 13:07:50 +0800

    [83]删除排序链表中的重复元素

--- src/Solution.java
+++ src/Solution.java
@@ -19,34 +19,16 @@ class ListNode {
 class Solution {
 
     public ListNode deleteDuplicates(ListNode head) {
-        head = findFirst(head);
         if (head == null) {
             return null;
         }
-
-        int repeatValue = 200;
         ListNode p = head;
         while (p.next != null) {
-            if (p.next.val == repeatValue || p.next.next != null && p.next.val == p.next.next.val) {
-                repeatValue = p.next.val;
+            if (p.val == p.next.val) {
                 p.next = p.next.next;
                 continue;
             }
             p = p.next;
-        }
-        return head;
-    }
-
-    private ListNode findFirst(ListNode head) {
-        ListNode pre = null;
-        while (head != null) {
-            int preVal = pre == null ? 200 : pre.val;
-            int headNextVal = head.next == null ? 200 : head.next.val;
-            if (head.val != preVal & head.val != headNextVal) {
-                break;
-            }
-            pre = head;
-            head = head.next;
         }
         return head;
     }
