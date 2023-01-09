244903a08ddec460722f8227e9a51672dfa2bfc0
yanglongqi <yanglongqi@xiaomi.com>
2023-01-09 18:48:51 +0800

    [85]最大矩形

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,43 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-
 class Solution {
 
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(right, n);
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                right[stack.peek()] = i;
-                stack.pop();
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] nums = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            nums[i][0] = matrix[i][0] - '0';
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                nums[i][j] = matrix[i][j] - '0';
+                if (nums[i][j] != 0) {
+                    nums[i][j] = nums[i][j - 1] + 1;
+                }
             }
-            left[i] = (stack.isEmpty() ? -1 : stack.peek());
-            stack.push(i);
+        }
+
+        int[][][] minFlag = new int[n][m][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = j; k < m; k++) {
+                    int min = nums[j][i];
+                    for (int col = j; col <= k; col++) {
+                        min = Math.min(min, nums[col][i]);
+                    }
+                    minFlag[i][j][k] = min;
+                }
+            }
         }
 
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        for (int col = 0; col < n; col++) {
+            for (int x1 = 0; x1 < m; x1++) {
+                for (int x2 = x1; x2 < m; x2++) {
+                    ans = Math.max(ans, minFlag[col][x1][x2] * (x2 - x1 + 1));
+                }
+            }
         }
         return ans;
-
     }
 }
