304b6b8c78bb783dd4adf10c2fd5e0a5c122c8d0
yanglongqi <yanglongqi@xiaomi.com>
2023-01-09 14:25:44 +0800

    [84]柱状图中最大的矩形

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,28 @@
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
 class Solution {
 
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        ListNode p = head;
-        while (p.next != null) {
-            if (p.val == p.next.val) {
-                p.next = p.next.next;
-                continue;
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                right[stack.peek()] = i;
+                stack.pop();
             }
-            p = p.next;
+            left[i] = (stack.isEmpty() ? -1 : stack.peek());
+            stack.push(i);
         }
-        return head;
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+
     }
 }
