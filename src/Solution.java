6b5988529720ae1059120694865f3313fdf8464b
Rainsheep <1045674971@qq.com>
2023-01-09 22:55:07 +0800

    [87]扰乱字符串

--- src/Solution.java
+++ src/Solution.java
@@ -1,54 +1,71 @@
-
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
-    public ListNode partition(ListNode head, int x) {
-        ListNode newHead = new ListNode();
-        newHead.next = head;
-        ListNode lessNode = newHead;
-        ListNode bigNode = null;
-        ListNode now = head;
-        ListNode pre = newHead;
-        while (now != null) {
-            if (now.val >= x) {
-                if (bigNode == null) {
-                    bigNode = now;
-                }
-                pre = now;
-                now = now.next;
-            } else {
-                if (bigNode == null) {
-                    pre = now;
-                    now = now.next;
-                } else {
-                    pre.next = now.next;
-                    now.next = lessNode.next;
-                    lessNode.next = now;
-                    now = pre.next;
-                }
-                lessNode = lessNode.next;
-            }
+    Map<String, Map<String, Boolean>> flagMap = new HashMap<>();
 
+    public boolean isScramble(String s1, String s2) {
+        return dfs(s1, s2);
+    }
 
+    private boolean dfs(String s1, String s2) {
+        Boolean mapRet = getMapRet(s1, s2);
+        if (mapRet != null) {
+            return mapRet;
         }
 
-        return newHead.next;
+        if (s1.equals(s2)) {
+            putMap(s1, s2, true);
+            return true;
+        }
+        int len = s1.length();
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> map3 = new HashMap<>();
+        for (int i = 0; i < len - 1; i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            char c3 = s2.charAt(len - 1 - i);
+            map1.put(c1, map1.getOrDefault(c1, 0) + 1);
+            map2.put(c2, map2.getOrDefault(c2, 0) + 1);
+            map3.put(c3, map3.getOrDefault(c3, 0) + 1);
+
+            if (map1.equals(map2)) {
+                boolean ret1 = dfs(s1.substring(0, i + 1), s2.substring(0, i + 1));
+                boolean ret2 = dfs(s1.substring(i + 1), s2.substring(i + 1));
+                if (ret1 && ret2) {
+                    putMap(s1, s2, true);
+                    return true;
+                }
+            }
+
+            if (map1.equals(map3)) {
+                boolean ret1 = dfs(s1.substring(0, i + 1), s2.substring(len - i - 1, len));
+                boolean ret2 = dfs(s1.substring(i + 1), s2.substring(0, len - i - 1));
+                if (ret1 && ret2) {
+                    putMap(s1, s2, true);
+                    return true;
+                }
+            }
+
+        }
+        putMap(s1, s2, false);
+        return false;
+
+    }
+
+    private Boolean getMapRet(String s1, String s2) {
+        Map<String, Boolean> map = flagMap.get(s1);
+        if (map == null) {
+            return null;
+        }
+        return map.get(s2);
+    }
+
+    private void putMap(String s1, String s2, boolean ret) {
+        Map<String, Boolean> map = flagMap.getOrDefault(s1, new HashMap<>());
+        map.put(s2, ret);
+        flagMap.put(s1, map);
     }
 }
