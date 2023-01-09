47c0137beaf7d92dc9ca6d7175b2d59c4fb097b3
Rainsheep <1045674971@qq.com>
2023-01-09 22:00:24 +0800

    [86]分隔链表

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,54 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
 
-    public int maximalRectangle(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] nums = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    nums[i][j] = (j == 0 ? 0 : nums[i][j - 1]) + 1;
+    public ListNode partition(ListNode head, int x) {
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        ListNode lessNode = newHead;
+        ListNode bigNode = null;
+        ListNode now = head;
+        ListNode pre = newHead;
+        while (now != null) {
+            if (now.val >= x) {
+                if (bigNode == null) {
+                    bigNode = now;
                 }
+                pre = now;
+                now = now.next;
+            } else {
+                if (bigNode == null) {
+                    pre = now;
+                    now = now.next;
+                } else {
+                    pre.next = now.next;
+                    now.next = lessNode.next;
+                    lessNode.next = now;
+                    now = pre.next;
+                }
+                lessNode = lessNode.next;
             }
+
+
         }
 
-        int ans = 0;
-        for (int col = 0; col < n; col++) {
-            int[] up = new int[m];
-            int[] down = new int[m];
-            Arrays.fill(down, m);
-            ArrayDeque<Integer> stack = new ArrayDeque<>();
-            for (int row = 0; row < m; row++) {
-                while (!stack.isEmpty() && nums[stack.peek()][col] >= nums[row][col]) {
-                    down[stack.peek()] = row;
-                    stack.pop();
-                }
-                up[row] = stack.isEmpty() ? -1 : stack.peek();
-                stack.push(row);
-            }
-
-            for (int row = 0; row < m; row++) {
-                ans = Math.max(ans, nums[row][col] * (down[row] - up[row] - 1));
-            }
-
-        }
-        return ans;
+        return newHead.next;
     }
 }
