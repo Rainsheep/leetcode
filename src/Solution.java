9480c4e38fd02560ed21eb58d5bca087972efbc3
yanglongqi <yanglongqi@xiaomi.com>
2023-01-09 11:18:43 +0800

    [81]搜索旋转排序数组 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,34 @@
-import java.util.HashMap;
-
 class Solution {
 
-    HashMap<Character, Integer> flagMap = new HashMap<>();
-    HashMap<Character, Integer> map = new HashMap<>();
-
-    public String minWindow(String s, String t) {
-        int leftAns = 0;
-        int rightAns = 0;
-
+    public boolean search(int[] nums, int target) {
         int left = 0;
-        int right = 0;
-
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            flagMap.put(c, flagMap.getOrDefault(c, 0) + 1);
-        }
-
-        while (right != s.length() + 1) {
-            if (check()) {
-                if (rightAns == 0 || right - left < rightAns - leftAns) {
-                    leftAns = left;
-                    rightAns = right;
-                }
-                char c = s.charAt(left);
-                map.put(c, map.getOrDefault(c, 1) - 1);
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
                 left++;
-            } else {
-                if (right == s.length()) {
-                    break;
-                }
-                char c = s.charAt(right);
-                map.put(c, map.getOrDefault(c, 0) + 1);
-                right++;
+                right--;
+                continue;
             }
-        }
-        return s.substring(leftAns, rightAns);
-    }
 
-    private boolean check() {
-        for (Character c : flagMap.keySet()) {
-            if (map.getOrDefault(c, 0) < flagMap.get(c)) {
-                return false;
+            if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-        return true;
+        return false;
     }
 }
