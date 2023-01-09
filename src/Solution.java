24e299d931f22a95e2ebb78b3a4c4605fc1a6cf3
yanglongqi <yanglongqi@xiaomi.com>
2023-01-09 13:04:44 +0800

    [82]删除排序链表中的重复元素 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,53 @@
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
 
-    public boolean search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int mid;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (nums[mid] == target) {
-                return true;
-            }
-            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
-                left++;
-                right--;
+    public ListNode deleteDuplicates(ListNode head) {
+        head = findFirst(head);
+        if (head == null) {
+            return null;
+        }
+
+        int repeatValue = 200;
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.next.val == repeatValue || p.next.next != null && p.next.val == p.next.next.val) {
+                repeatValue = p.next.val;
+                p.next = p.next.next;
                 continue;
             }
-
-            if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
+            p = p.next;
         }
-        return false;
+        return head;
+    }
+
+    private ListNode findFirst(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            int preVal = pre == null ? 200 : pre.val;
+            int headNextVal = head.next == null ? 200 : head.next.val;
+            if (head.val != preVal & head.val != headNextVal) {
+                break;
+            }
+            pre = head;
+            head = head.next;
+        }
+        return head;
     }
 }
