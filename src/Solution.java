596f0baa2743add3873b80a2474505900ce00bb9
yanglongqi <yanglongqi@xiaomi.com>
2023-01-10 13:20:30 +0800

    [90]子集 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,32 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 class Solution {
 
-    HashSet<List<Integer>> set = new HashSet<>();
+    List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        dfs(nums, 0, new ArrayDeque<>());
-        return new ArrayList<>(set);
+        dfs(nums, 0, new ArrayDeque<>(), false);
+        return ans;
     }
 
-    private void dfs(int[] nums, int index, ArrayDeque<Integer> list) {
+    private void dfs(int[] nums, int index, ArrayDeque<Integer> list, boolean last) {
         if (index == nums.length) {
-            set.add(new ArrayList<>(list));
+            ans.add(new ArrayList<>(list));
             return;
         }
 
-        dfs(nums, index + 1, list);
+        if (index > 0 && nums[index] == nums[index - 1] && !last) {
+            dfs(nums, index + 1, list, false);
+            return;
+        }
+
+        dfs(nums, index + 1, list, false);
         list.add(nums[index]);
-        dfs(nums, index + 1, list);
+        dfs(nums, index + 1, list, true);
         list.removeLast();
     }
 }
