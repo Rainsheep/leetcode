3011f0febbf8c8360d308ff2efc59b4e927406c2
yanglongqi <yanglongqi@xiaomi.com>
2023-01-10 13:03:49 +0800

    [90]子集 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -9,21 +9,20 @@ class Solution {
     HashSet<List<Integer>> set = new HashSet<>();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         dfs(nums, 0, new ArrayDeque<>());
         return new ArrayList<>(set);
     }
 
     private void dfs(int[] nums, int index, ArrayDeque<Integer> list) {
         if (index == nums.length) {
-            ArrayList<Integer> t = new ArrayList<>(list);
-            Collections.sort(t);
-            set.add(t);
+            set.add(new ArrayList<>(list));
             return;
         }
 
+        dfs(nums, index + 1, list);
         list.add(nums[index]);
         dfs(nums, index + 1, list);
         list.removeLast();
-        dfs(nums, index + 1, list);
     }
 }
