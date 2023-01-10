f25e2c2360e7a7dbab1dd3eae62e5e5c01385824
yanglongqi <yanglongqi@xiaomi.com>
2023-01-10 10:40:03 +0800

    [复习][85]最大矩形

--- src/Solution.java
+++ src/Solution.java
@@ -3,26 +3,38 @@ import java.util.Arrays;
 
 class Solution {
 
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(right, n);
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                right[stack.peek()] = i;
-                stack.pop();
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] left = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
             }
-            left[i] = (stack.isEmpty() ? -1 : stack.peek());
-            stack.push(i);
         }
 
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        for (int col = 0; col < n; col++) {
+            int[] up = new int[m];
+            int[] down = new int[m];
+            Arrays.fill(down, m);
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            for (int row = 0; row < m; row++) {
+                while (!stack.isEmpty() && left[stack.peek()][col] >= left[row][col]) {
+                    down[stack.peek()] = row;
+                    stack.pop();
+                }
+
+                up[row] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(row);
+            }
+
+            for (int row = 0; row < m; row++) {
+                ans = Math.max(ans, (down[row] - up[row] - 1) * left[row][col]);
+            }
         }
         return ans;
-
     }
 }
