35d8314a447591e70836538d7562b6bcc01e5625
yanglongqi <yanglongqi@xiaomi.com>
2023-01-12 19:18:13 +0800

    [91]解码方法

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
 
-    List<List<Integer>> ans = new ArrayList<>();
+    public int numDecodings(String s) {
+        int len = s.length();
+        if (len == 0 || s.startsWith("0")) {
+            return 0;
+        }
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= len; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
 
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        dfs(nums, 0, true, new ArrayDeque<>());
-        return ans;
-    }
-
-    private void dfs(int[] nums, int index, boolean last, ArrayDeque<Integer> list) {
-        if (index == nums.length) {
-            ans.add(new ArrayList<>(list));
-            return;
+            if (i > 1 && s.charAt(i - 2) != '0') {
+                int num = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+                if (num > 0 && num < 27) {
+                    dp[i] += dp[i - 2];
+                }
+            }
         }
 
-        if (!last && nums[index] == nums[index - 1]) {
-            dfs(nums, index + 1, false, list);
-            return;
-        }
-
-        dfs(nums, index + 1, false, list);
-        list.addLast(nums[index]);
-        dfs(nums, index + 1, true, list);
-        list.removeLast();
+        return dp[len];
     }
-
 }
