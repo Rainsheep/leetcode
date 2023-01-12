c67485c51f695899dc6c90e5e3cad9a56ceab5fe
yanglongqi <yanglongqi@xiaomi.com>
2023-01-12 19:44:26 +0800

    [92]反转链表 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,57 @@
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
 
-    public int numDecodings(String s) {
-        int len = s.length();
-        if (len == 0 || s.startsWith("0")) {
-            return 0;
-        }
-        int[] dp = new int[len + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= len; i++) {
-            if (s.charAt(i - 1) != '0') {
-                dp[i] += dp[i - 1];
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        ListNode p = newHead;
+        ListNode beginPre = null;
+        ListNode begin = null;
+        ListNode pre = null;
+        ListNode next;
+        for (int i = 0; i <= right; i++) {
+            if (i == left - 1) {
+                beginPre = p;
             }
-
-            if (i > 1 && s.charAt(i - 2) != '0') {
-                int num = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
-                if (num > 0 && num < 27) {
-                    dp[i] += dp[i - 2];
+            if (i == left) {
+                begin = p;
+            }
+            if (i > left) {
+                next = p.next;
+                p.next = pre;
+                if (i == right) {
+                    beginPre.next = p;
+                    begin.next = next;
+                    break;
                 }
+
+                pre = p;
+                p = next;
+
+                continue;
             }
+            pre = p;
+            p = p.next;
+
         }
 
-        return dp[len];
+        return newHead.next;
     }
 }
