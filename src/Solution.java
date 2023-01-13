be9726e676341c383a3ba0aa57d1b0776669c94a
yanglongqi <yanglongqi@xiaomi.com>
2023-01-13 14:24:54 +0800

    [97]交错字符串

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,28 @@
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
 
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= i - 1; j++) {
-                dp[i] += dp[j] * dp[i - 1 - j];
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int s3Len = s3.length();
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+        if (s1Len + s2Len != s3Len) {
+            return false;
+        }
+        boolean[] dp = new boolean[s2Len + 1];
+
+        for (int i = 0; i <= s1Len; i++) {
+            for (int j = 0; j <= s2Len; j++) {
+                int p = i + j - 1;
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(p);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(p);
+                } else {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(p) || dp[j - 1] && s2.charAt(j - 1) == s3.charAt(p);
+                }
             }
         }
-        return dp[n];
+        return dp[s2Len];
     }
 }
