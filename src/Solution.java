e0144054c25f68e8938b9a257e85df75001fb506
yanglongqi <yanglongqi@xiaomi.com>
2023-01-13 14:33:57 +0800

    [98]验证二叉搜索树

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,45 @@
+import java.util.ArrayList;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
 
-    public boolean isInterleave(String s1, String s2, String s3) {
-        int s3Len = s3.length();
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-        if (s1Len + s2Len != s3Len) {
-            return false;
-        }
-        boolean[] dp = new boolean[s2Len + 1];
+    ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i <= s1Len; i++) {
-            for (int j = 0; j <= s2Len; j++) {
-                int p = i + j - 1;
-                if (i == 0 && j == 0) {
-                    dp[j] = true;
-                } else if (i == 0) {
-                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(p);
-                } else if (j == 0) {
-                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(p);
-                } else {
-                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(p) || dp[j - 1] && s2.charAt(j - 1) == s3.charAt(p);
-                }
+    public boolean isValidBST(TreeNode root) {
+        dfs(root);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
             }
         }
-        return dp[s2Len];
+        return true;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
     }
 }
