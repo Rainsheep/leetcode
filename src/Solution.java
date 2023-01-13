101ff0cb87980711fa300792a40a75492d002c7b
yanglongqi <yanglongqi@xiaomi.com>
2023-01-13 10:57:19 +0800

    [95]不同的二叉搜索树 II

--- src/Solution.java
+++ src/Solution.java
@@ -24,20 +24,83 @@ class TreeNode {
 
 class Solution {
 
-    ArrayList<Integer> ans = new ArrayList<>();
+    List<TreeNode> ans = new ArrayList<>();
+    boolean[] flag;
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        dfs(root);
+    public List<TreeNode> generateTrees(int n) {
+        flag = new boolean[n + 1];
+        dfs(0, n, null);
         return ans;
-
     }
 
-    private void dfs(TreeNode root) {
-        if (root == null) {
+    private void dfs(int index, int n, TreeNode tree) {
+        if (index == n) {
+            insertList(tree);
             return;
         }
-        ans.add(root.val);
-        dfs(root.left);
-        dfs(root.right);
+        for (int i = 1; i <= n; i++) {
+            if (!flag[i]) {
+                flag[i] = true;
+                TreeNode copyTree = copyTree(tree);
+                copyTree = insertNode(copyTree, i);
+                dfs(index + 1, n, copyTree);
+                flag[i] = false;
+            }
+        }
+
     }
+
+    private TreeNode insertNode(TreeNode tree, int num) {
+        if (tree == null) {
+            return new TreeNode(num);
+        }
+
+        TreeNode pre = null;
+        TreeNode p = tree;
+        while (p != null) {
+            pre = p;
+            p = num > p.val ? p.right : p.left;
+        }
+        if (num > pre.val) {
+            pre.right = new TreeNode(num);
+        } else {
+            pre.left = new TreeNode(num);
+        }
+
+        return tree;
+    }
+
+    private boolean isEqualsTree(TreeNode tree1, TreeNode tree2) {
+        if (tree1 == null && tree2 == null) {
+            return true;
+        }
+        if (tree1 == null || tree2 == null) {
+            return false;
+        }
+        if (tree1.val != tree2.val) {
+            return false;
+        }
+        return isEqualsTree(tree1.left, tree2.left) && isEqualsTree(tree1.right, tree2.right);
+    }
+
+    private void insertList(TreeNode tree) {
+        for (TreeNode an : ans) {
+            if (isEqualsTree(an, tree)) {
+                return;
+            }
+        }
+        ans.add(tree);
+    }
+
+    private TreeNode copyTree(TreeNode treeNode) {
+        if (treeNode == null) {
+            return null;
+        }
+
+        TreeNode res = new TreeNode(treeNode.val);
+        res.left = copyTree(treeNode.left);
+        res.right = copyTree(treeNode.right);
+        return res;
+    }
+
 }
