38f3adafebb56d92a413aa5ec54cee45dd51225f
yanglongqi <yanglongqi@xiaomi.com>
2023-01-16 19:22:53 +0800

    [复习][91]解码方法

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,22 @@
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
+    public int numDecodings(String s) {
+        int sLen = s.length();
+        if (sLen == 0 || s.startsWith("0")) {
+            return 0;
         }
-        if (p == null || q == null) {
-            return false;
+        int[] dp = new int[sLen + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= sLen; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            int t = i >= 2 ? (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') : 0;
+            if (t > 9 && t < 27) {
+                dp[i] += dp[i - 2];
+            }
         }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return dp[sLen];
     }
 }
