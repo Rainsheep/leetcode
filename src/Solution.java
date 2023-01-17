572427435110eb307d3177749b0798509a128cde
yanglongqi <yanglongqi@xiaomi.com>
2023-01-17 16:47:16 +0800

    [复习][31]下一个排列

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,34 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
 
-    ArrayList<List<Integer>> ans = new ArrayList<>();
-    ArrayDeque<Integer> list = new ArrayDeque<>();
-
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        dfs(0, true, nums);
-        return ans;
-    }
-
-    private void dfs(int index, boolean pre, int[] nums) {
-        if (nums.length == index) {
-            ans.add(new ArrayList<>(list));
+    public void nextPermutation(int[] nums) {
+        if (nums.length == 1) {
             return;
         }
 
-        if (!pre && nums[index] == nums[index - 1]) {
-            dfs(index + 1, false, nums);
+        int k = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+
+        if (k == -1) {
+            Arrays.sort(nums);
             return;
         }
 
-        dfs(index + 1, false, nums);
+        for (int i = nums.length - 1; i > k; i--) {
+            if (nums[i] > nums[k]) {
+                int t = nums[i];
+                nums[i] = nums[k];
+                nums[k] = t;
+                break;
+            }
+        }
 
-        list.add(nums[index]);
-        dfs(index + 1, true, nums);
-        list.removeLast();
+        Arrays.sort(nums, k + 1, nums.length);
     }
 }
