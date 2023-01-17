b91963eed654069f270a1b9b0a963f87249aa811
yanglongqi <yanglongqi@xiaomi.com>
2023-01-17 18:27:12 +0800

    [复习][45]跳跃游戏 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,24 @@
 class Solution {
 
-    public int firstMissingPositive(int[] nums) {
+    public int jump(int[] nums) {
+        int maxIndex = 0;
+        int step = 0;
+        int nextMax = 0;
         int len = nums.length;
 
-        for (int i = 0; i < len; i++) {
-            while (nums[i] > 0 && nums[i] <= len && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
+        for (int i = 0; i < len - 1; i++) {
+            nextMax = Math.max(nextMax, i + nums[i]);
+
+            if (nextMax >= len - 1) {
+                return step + 1;
+            }
+
+            if (maxIndex == i) {
+                maxIndex = nextMax;
+                step++;
             }
         }
+        return 0;
 
-        int ret = len + 1;
-        for (int i = 0; i < len; i++) {
-            if (nums[i] != i + 1) {
-                ret = i + 1;
-                break;
-            }
-        }
-        return ret;
-    }
-
-
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
     }
 }
