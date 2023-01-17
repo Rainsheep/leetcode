592d6d8d524eed162e7c6c78a5225e94bd20ac61
yanglongqi <yanglongqi@xiaomi.com>
2023-01-17 14:56:43 +0800

    [复习][73]矩阵置零

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,61 @@
 class Solution {
 
-    public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0) {
-                    dp[0][j] = j;
-                } else if (j == 0) {
-                    dp[i][0] = i;
-                } else {
-                    int up = dp[i - 1][j] + 1;
-                    int left = dp[i][j - 1] + 1;
-                    int leftUp = dp[i - 1][j - 1];
-                    if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                        leftUp++;
-                    }
-                    dp[i][j] = Math.min(up, Math.min(leftUp, left));
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowFlag = false;
+        boolean firstColFlag = false;
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowFlag = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColFlag = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        return dp[m][n];
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < m; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        if (firstRowFlag) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if (firstColFlag) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
     }
 }
