6fce7f33eb3cd4f4fc31febcae507be445fc7b97
yanglongqi <yanglongqi@xiaomi.com>
2023-01-17 15:18:07 +0800

    [复习][76]最小覆盖子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,61 +1,52 @@
+import java.util.HashMap;
+
 class Solution {
 
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean firstRowFlag = false;
-        boolean firstColFlag = false;
+    HashMap<Character, Integer> flagMap = new HashMap<>();
+    HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                firstRowFlag = true;
+    public String minWindow(String s, String t) {
+
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            flagMap.put(c, flagMap.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int right = -1;
+        int leftAns = 0;
+        int rightAns = -1;
+        while (true) {
+            while (check()) {
+                if (rightAns == -1 || right - left < rightAns - leftAns) {
+                    leftAns = left;
+                    rightAns = right;
+                }
+
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                left++;
+            }
+
+            right++;
+            if (right == s.length()) {
                 break;
             }
+            char rightChar = s.charAt(right);
+            map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
         }
 
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColFlag = true;
-                break;
+        return s.substring(leftAns, rightAns + 1);
+    }
+
+
+    private boolean check() {
+        for (Character character : flagMap.keySet()) {
+            if (map.getOrDefault(character, 0) < flagMap.get(character)) {
+                return false;
             }
         }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for (int i = 1; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 1; j < m; j++) {
-                    matrix[j][i] = 0;
-                }
-            }
-        }
-
-        if (firstRowFlag) {
-            for (int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-
-        if (firstColFlag) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-
+        return true;
     }
 }
