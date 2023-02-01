c07a36d2b7584dccbc628c27065572f4a5e3470e
yanglongqi <yanglongqi@xiaomi.com>
2023-02-01 19:06:40 +0800

    [104]二叉树的最大深度

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,3 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-
 class TreeNode {
 
     int val;
@@ -24,31 +20,10 @@ class TreeNode {
 
 class Solution {
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        ArrayList<List<Integer>> ret = new ArrayList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
-        int size = 0;
-        if (root != null) {
-            queue.add(root);
-            size = 1;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.pop();
-            if (node.left != null) {
-                queue.addLast(node.left);
-            }
-            if (node.right != null) {
-                queue.addLast(node.right);
-            }
-            list.add(node.val);
-            if (list.size() == size || queue.isEmpty()) {
-                size = queue.size();
-                ret.add(list);
-                list = new ArrayList<>();
-            }
-        }
-        return ret;
-
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
