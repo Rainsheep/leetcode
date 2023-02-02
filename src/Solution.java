9a4d853e84904a9e4a4e1017a29e096863558a03
yanglongqi <yanglongqi@xiaomi.com>
2023-02-02 19:58:55 +0800

    [121]买卖股票的最佳时机

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,12 @@
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
-    public void flatten(TreeNode root) {
-        TreeNode curr = root;
-        while (curr != null) {
-            if (curr.left != null) {
-                TreeNode next = curr.left;
-                TreeNode preNode = next;
-                while (preNode.right != null) {
-                    preNode = preNode.right;
-                }
 
-                preNode.right = curr.right;
-                curr.left = null;
-                curr.right = next;
-            }
-            curr = curr.right;
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int min = prices[0];
+        for (int price : prices) {
+            min = Math.min(min, price);
+            ans = Math.max(ans, price - min);
         }
+        return ans;
     }
 }
