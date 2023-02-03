c4bd2606b66fb9315d1dc96d610b4fdb868d5ead
yanglongqi <yanglongqi@xiaomi.com>
2023-02-03 10:29:45 +0800

    [124]二叉树中的最大路径和

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,50 @@
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
 
-    public int maxProfit(int[] prices) {
-        int ans = 0;
-        int min = prices[0];
-        for (int price : prices) {
-            min = Math.min(min, price);
-            ans = Math.max(ans, price - min);
-        }
+    int ans;
+
+    public int maxPathSum(TreeNode root) {
+        ans = root.val;
+        dfs(root);
         return ans;
+
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+
+        int leftMax = dfs(root.left);
+        int rightMax = dfs(root.right);
+        int maxVal = Math.max(leftMax, rightMax);
+        int res = root.val + Math.max(maxVal, 0);
+
+        if (leftMax > 0 && rightMax > 0) {
+            ans = Math.max(ans, leftMax + rightMax + root.val);
+        } else {
+            ans = Math.max(ans, res);
+        }
+
+        return res;
     }
 }
