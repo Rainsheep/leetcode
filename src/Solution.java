4b8edea95bf026d64ddea21e848fc2f23b406247
yanglongqi <yanglongqi@xiaomi.com>
2023-02-03 12:21:01 +0800

    [139]单词拆分

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,24 @@
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int num : nums) {
-            ans ^= num;
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j <= i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
-        return ans;
+
+        return dp[len];
     }
 }
