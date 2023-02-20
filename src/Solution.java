fa8e0c860951b93942c8f688634fe88123b93a24
yanglongqi <yanglongqi@xiaomi.com>
2023-02-20 10:32:25 +0800

    [复习][136]只出现一次的数字

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,10 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
 
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+    public int singleNumber(int[] nums) {
+        int ret = 0;
         for (int num : nums) {
-            set.add(num);
+            ret = ret ^ num;
         }
-
-        int ans = 0;
-        for (int num : nums) {
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStack = 1;
-                while (set.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStack += 1;
-                }
-                ans = Math.max(ans, currentStack);
-            }
-        }
-        return ans;
+        return ret;
     }
 }
