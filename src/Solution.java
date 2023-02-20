506afc8b0f23d6e811de127581cb93dd891e400c
yanglongqi <yanglongqi@xiaomi.com>
2023-02-20 15:07:39 +0800

    [146]LRU 缓存

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,85 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+public class LRUCache {
 
-    private int capacity;
+    private static class Node {
+
+        int key;
+        int value;
+        Node pre;
+        Node next;
+
+        public Node() {
+        }
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    HashMap<Integer, Node> cache = new HashMap<>();
+    int capacity;
+    Node head;
+    Node tail;
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
         this.capacity = capacity;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.pre = head;
     }
 
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            moveToLast(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    private void moveToLast(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+
+        addToTail(node);
     }
 
     public void put(int key, int value) {
-        super.put(key, value);
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToLast(node);
+            return;
+        }
+
+        Node node = new Node(key, value);
+        addToTail(node);
+        cache.put(key, node);
+
+        if (cache.size() > capacity) {
+            int removeKey = removeHead();
+            cache.remove(removeKey);
+        }
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    private void addToTail(Node node) {
+        tail.pre.next = node;
+
+        node.pre = tail.pre;
+        node.next = tail;
+
+        tail.pre = node;
     }
+
+    private int removeHead() {
+        Node node = head.next;
+        head.next = node.next;
+        node.next.pre = head;
+        return node.key;
+    }
+
+
 }
