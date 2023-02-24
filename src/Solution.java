00d4a67df6a10f63cffbd9eb60038e7f98cf0c55
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 15:39:46 +0800

    [复习][146]LRU 缓存

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,78 @@
+import java.util.HashMap;
 
-class ListNode {
+class LRUCache {
 
-    int val;
-    ListNode next;
+    static class Node {
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+        int key;
+        int value;
+        Node next;
+        Node pre;
+
+        public Node() {
+        }
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
-}
 
-public class Solution {
+    HashMap<Integer, Node> map;
+    Node head;
+    Node tail;
+    int capacity;
 
-    public ListNode detectCycle(ListNode head) {
-        ListNode fast = head, slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                break;
-            }
+    public LRUCache(int capacity) {
+        map = new HashMap<>(capacity);
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.pre = head;
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
         }
-        if (fast == null || fast.next == null) {
-            return null;
-        }
+        Node node = map.get(key);
+        moveToTail(node);
+        return node.value;
+    }
 
-        fast = head;
-        while (fast != slow) {
-            fast = fast.next;
-            slow = slow.next;
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            moveToTail(node);
+        } else if (map.size() == capacity) {
+            removeHead();
+            Node node = new Node(key, value);
+            addTail(node);
+        } else {
+            Node node = new Node(key, value);
+            addTail(node);
         }
-        return slow;
+    }
 
+    private void removeHead() {
+        map.remove(head.next.key);
+        head.next = head.next.next;
+        head.next.pre = head;
+    }
+
+    private void addTail(Node node) {
+        map.put(node.key, node);
+        node.next = tail;
+        node.pre = tail.pre;
+        tail.pre.next = node;
+        tail.pre = node;
+    }
+
+    private void moveToTail(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+        addTail(node);
     }
 }
