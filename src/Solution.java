0cc5d3e3a682af5c855ea9d50378245e3c36858f
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 19:22:28 +0800

    [206]反转链表

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,36 @@
-class Solution {
+class ListNode {
 
-    int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    int val;
+    ListNode next;
 
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] flag = new boolean[m][n];
-        int ret = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !flag[i][j]) {
-                    ret++;
-                    dfs(grid, flag, m, n, i, j);
-                }
-            }
-        }
-        return ret;
-
+    ListNode() {
     }
 
-    private void dfs(char[][] grid, boolean[][] flag, int m, int n, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || flag[i][j] || grid[i][j] == '0') {
-            return;
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
         }
-        flag[i][j] = true;
-        for (int[] d : dir) {
-            dfs(grid, flag, m, n, i + d[0], j + d[1]);
+        ListNode pre = null;
+        ListNode p = head;
+        ListNode next;
+        while (p != null) {
+            next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
         }
+        return pre;
     }
 }
