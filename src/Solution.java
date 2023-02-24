e6b76d19c9c49b2fed778b2bd2320f05dd64b60c
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 18:58:15 +0800

    [200]岛屿数量

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,31 @@
 class Solution {
 
-    public int rob(int[] nums) {
-        int len = nums.length;
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-        for (int i = 1; i < len; i++) {
-            int t = i >= 2 ? dp[i - 2] : 0;
-            dp[i] = Math.max(t + nums[i], dp[i - 1]);
+    int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] flag = new boolean[m][n];
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !flag[i][j]) {
+                    ret++;
+                    dfs(grid, flag, m, n, i, j);
+                }
+            }
         }
-        return dp[len - 1];
+        return ret;
+
+    }
+
+    private void dfs(char[][] grid, boolean[][] flag, int m, int n, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || flag[i][j] || grid[i][j] == '0') {
+            return;
+        }
+        flag[i][j] = true;
+        for (int[] d : dir) {
+            dfs(grid, flag, m, n, i + d[0], j + d[1]);
+        }
     }
 }
