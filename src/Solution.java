1f77bced1aa333121bc463508cea5106e755275d
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 14:42:56 +0800

    [复习][128]最长连续序列

--- src/Solution.java
+++ src/Solution.java
@@ -1,66 +1,23 @@
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import java.util.HashSet;
 
 class Solution {
 
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
 
-        ListNode slow = head, fast = head.next;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+        int res = 0;
+        for (int num : nums) {
+            int len = 1;
+            int nowNum = num;
+            while (set.contains(nowNum + 1)) {
+                len++;
+                nowNum++;
+            }
+            res = Math.max(res, len);
         }
-        ListNode mid = slow.next;
-        slow.next = null;
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        return merge(left, right);
-    }
-
-    private ListNode merge(ListNode a, ListNode b) {
-        ListNode newHead = new ListNode();
-        ListNode p = newHead;
-        while (a != null || b != null) {
-            if (a == null) {
-                p.next = b;
-                break;
-            }
-
-            if (b == null) {
-                p.next = a;
-                break;
-            }
-
-            if (a.val <= b.val) {
-                p.next = a;
-                a = a.next;
-            } else {
-                p.next = b;
-                b = b.next;
-
-            }
-            p = p.next;
-        }
-
-        return newHead.next;
-
+        return res;
     }
 }
