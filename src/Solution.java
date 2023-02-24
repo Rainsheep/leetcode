0eb2ee44900ec55ca5591b77dffc41ebc31078c3
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 16:48:27 +0800

    [152]乘积最大子数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,63 +1,20 @@
-
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 class Solution {
 
-    public ListNode sortList(ListNode head) {
-        return sort(head);
-    }
-
-    private ListNode sort(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode fast = head.next, slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        ListNode second = sort(slow.next);
-        slow.next = null;
-        ListNode first = sort(head);
-        return merge(first, second);
-    }
-
-    private ListNode merge(ListNode first, ListNode second) {
-        ListNode head = new ListNode();
-        ListNode p = head;
-        while (first != null && second != null) {
-            if (first.val < second.val) {
-                p.next = first;
-                p = p.next;
-                first = first.next;
-            } else {
-                p.next = second;
-                p = p.next;
-                second = second.next;
-            }
+    public int maxProduct(int[] nums) {
+        int length = nums.length;
+        int[] max = new int[length];
+        int[] min = new int[length];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        for (int i = 1; i < length; i++) {
+            max[i] = Math.max(max[i - 1] * nums[i], Math.max(nums[i], min[i - 1] * nums[i]));
+            min[i] = Math.min(max[i - 1] * nums[i], Math.min(nums[i], min[i - 1] * nums[i]));
         }
 
-        if (first == null) {
-            p.next = second;
-        } else {
-            p.next = first;
+        int ans = max[0];
+        for (int i : max) {
+            ans = Math.max(i, ans);
         }
-        return head.next;
+        return ans;
     }
 }
