f22b8dac3635217ee6d7d1841d69bb989fc84069
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 15:05:27 +0800

    [复习][142]环形链表 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,36 @@
-import java.util.HashSet;
 
-class Solution {
+class ListNode {
 
-    public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
+    int val;
+    ListNode next;
 
-        int res = 0;
-        for (int num : nums) {
-            int len = 1;
-            int nowNum = num;
-            while (set.contains(nowNum + 1)) {
-                len++;
-                nowNum++;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
             }
-            res = Math.max(res, len);
         }
-        return res;
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+
     }
 }
