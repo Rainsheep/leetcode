3d4fca7878dc531fe528612953999185e785340a
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 17:32:15 +0800

    [160]相交链表

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,25 @@
-import java.util.ArrayDeque;
+class ListNode {
 
-class MinStack {
+    int val;
+    ListNode next;
 
-    ArrayDeque<Integer> stack;
-    ArrayDeque<Integer> min;
-
-    public MinStack() {
-        stack = new ArrayDeque<>();
-        min = new ArrayDeque<>();
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
+}
 
-    public void push(int val) {
-        stack.push(val);
-        min.push(Math.min(val, min.isEmpty() ? val : min.getFirst()));
-    }
+public class Solution {
 
-    public void pop() {
-        stack.pop();
-        min.pop();
-    }
-
-    public int top() {
-        return stack.peek();
-    }
-
-    public int getMin() {
-        return min.peek();
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
     }
 }
