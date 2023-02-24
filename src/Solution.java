023d7babc95b4cbe52638a1f00cc48d4b26f635b
yanglongqi <yanglongqi@xiaomi.com>
2023-02-24 15:49:53 +0800

    [复习][146]LRU 缓存

--- src/Solution.java
+++ src/Solution.java
@@ -1,78 +1,23 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
-class LRUCache {
+class LRUCache extends LinkedHashMap<Integer, Integer> {
 
-    static class Node {
 
-        int key;
-        int value;
-        Node next;
-        Node pre;
-
-        public Node() {
-        }
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-    HashMap<Integer, Node> map;
-    Node head;
-    Node tail;
     int capacity;
 
     public LRUCache(int capacity) {
-        map = new HashMap<>(capacity);
-        head = new Node();
-        tail = new Node();
-        head.next = tail;
-        tail.pre = head;
+        super(capacity, 0.75f, true);
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        if (!map.containsKey(key)) {
-            return -1;
-        }
-        Node node = map.get(key);
-        moveToTail(node);
-        return node.value;
+        return this.getOrDefault(key, -1);
     }
 
-    public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            node.value = value;
-            moveToTail(node);
-        } else if (map.size() == capacity) {
-            removeHead();
-            Node node = new Node(key, value);
-            addTail(node);
-        } else {
-            Node node = new Node(key, value);
-            addTail(node);
-        }
-    }
 
-    private void removeHead() {
-        map.remove(head.next.key);
-        head.next = head.next.next;
-        head.next.pre = head;
-    }
-
-    private void addTail(Node node) {
-        map.put(node.key, node);
-        node.next = tail;
-        node.pre = tail.pre;
-        tail.pre.next = node;
-        tail.pre = node;
-    }
-
-    private void moveToTail(Node node) {
-        node.pre.next = node.next;
-        node.next.pre = node.pre;
-        addTail(node);
+    @Override
+    protected boolean removeEldestEntry(Entry<Integer, Integer> eldest) {
+        return this.size() > capacity;
     }
 }
