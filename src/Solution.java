6436a8db676de9b97cf7139d0125e509b6b226ae
yanglongqi <yanglongqi@xiaomi.com>
2023-02-27 19:39:08 +0800

    [215]数组中的第K个最大元素

--- src/Solution.java
+++ src/Solution.java
@@ -1,71 +1,41 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
-class Trie {
+class Solution {
 
-    class Node {
-
-        Character c;
-        boolean isNode;
-        Map<Character, Node> list = new HashMap<>();
-
-        public Node(Character c) {
-            this.c = c;
-        }
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
     }
 
-    Node root;
-
-    public Trie() {
-        this.root = new Node(null);
+    public int quickSelect(int[] nums, int l, int r, int index) {
+        int q = randomPartition(nums, l, r);
+        if (q == index) {
+            return nums[q];
+        }
+        return q < index ? quickSelect(nums, q + 1, r, index) : quickSelect(nums, l, q - 1, index);
     }
 
-    public void insert(String word) {
-        Node p = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (p.list.containsKey(c)) {
-                p = p.list.get(c);
-            } else {
-                Node next = new Node(c);
-                p.list.put(c, next);
-                p = next;
-            }
-            if (i == word.length() - 1) {
-                p.isNode = true;
-            }
-        }
+    public int randomPartition(int[] nums, int l, int r) {
+        int i = new Random().nextInt(r - l + 1) + l;
+        swap(nums, i, l);
+        return partition(nums, l, r);
+
     }
 
-    public boolean search(String word) {
-        Node p = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (p.list.containsKey(c)) {
-                p = p.list.get(c);
-            } else {
-                return false;
-            }
-
-            if (i == word.length() - 1) {
-                return p.isNode;
+    private int partition(int[] nums, int l, int r) {
+        int x = nums[l];
+        int k = l - 1;
+        for (int i = l; i <= r; i++) {
+            if (nums[i] <= x) {
+                swap(nums, ++k, i);
             }
         }
-        return true;
+        swap(nums, k, l);
+        return k;
     }
 
-    public boolean startsWith(String prefix) {
-        Node p = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            char c = prefix.charAt(i);
-            if (p.list.containsKey(c)) {
-                p = p.list.get(c);
-            } else {
-                return false;
-            }
-        }
-        return true;
-
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
-
