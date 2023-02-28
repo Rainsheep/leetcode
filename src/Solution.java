c75582568c260797da48044889e6076c9c43cefa
yanglongqi <yanglongqi@xiaomi.com>
2023-02-28 10:22:03 +0800

    [复习][207]课程表

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
-class ListNode {
+class Solution {
 
-    ListNode next;
-}
+    boolean ans = true;
 
-public class Solution {
-
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) {
-            return null;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] in = new int[numCourses];
+        int num = 0;
+        for (int[] prerequisite : prerequisites) {
+            List<Integer> list = map.getOrDefault(prerequisite[0], new ArrayList<>());
+            list.add(prerequisite[1]);
+            map.put(prerequisite[0], list);
+            in[prerequisite[1]]++;
         }
 
-        ListNode pA = headA;
-        ListNode pB = headB;
-        while (pA != pB) {
-            pA = pA == null ? headB : pA.next;
-            pB = pB == null ? headA : pB.next;
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) {
+                queue.add(i);
+            }
         }
-        return pA;
+
+        while (!queue.isEmpty()) {
+            num++;
+            Integer top = queue.pop();
+            List<Integer> list = map.get(top);
+            if (list != null) {
+                for (Integer integer : list) {
+                    in[integer]--;
+                    if (in[integer] == 0) {
+                        queue.add(integer);
+                    }
+                }
+            }
+        }
+
+        return num == numCourses;
     }
+
 }
