bf2b8b608eed36bccfab7ae21974505f96ddde3a
yanglongqi <yanglongqi@xiaomi.com>
2023-02-28 10:13:39 +0800

    [复习][160]相交链表

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,22 @@
-import java.util.Random;
 
-class Solution {
+class ListNode {
 
-    public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
-    }
+    ListNode next;
+}
 
-    public int quickSelect(int[] nums, int l, int r, int index) {
-        int q = randomPartition(nums, l, r);
-        if (q == index) {
-            return nums[q];
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
         }
-        return q < index ? quickSelect(nums, q + 1, r, index) : quickSelect(nums, l, q - 1, index);
-    }
 
-    public int randomPartition(int[] nums, int l, int r) {
-        int i = new Random().nextInt(r - l + 1) + l;
-        swap(nums, i, l);
-        return partition(nums, l, r);
-
-    }
-
-    private int partition(int[] nums, int l, int r) {
-        int x = nums[l];
-        int k = l - 1;
-        for (int i = l; i <= r; i++) {
-            if (nums[i] <= x) {
-                swap(nums, ++k, i);
-            }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
         }
-        swap(nums, k, l);
-        return k;
-    }
-
-    private void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        return pA;
     }
 }
