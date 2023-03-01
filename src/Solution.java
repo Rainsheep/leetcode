459c8f8e53aba20ee6cbf9b8dbfeb64b8be615ff
yanglongqi <yanglongqi@xiaomi.com>
2023-03-01 19:03:21 +0800

    [240]搜索二维矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -4,8 +4,9 @@ class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         int len = matrix.length;
+        int n = matrix[0].length;
         for (int[] ints : matrix) {
-            if (find(ints, target)) {
+            if (ints[n - 1] >= target && find(ints, target)) {
                 return true;
             }
         }
