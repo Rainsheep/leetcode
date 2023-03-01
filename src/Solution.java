6e0d2231336b5b9d2a799f793482f8fd53a76699
yanglongqi <yanglongqi@xiaomi.com>
2023-03-01 17:09:43 +0800

    [239]滑动窗口最大值

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,31 @@
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 
 class Solution {
 
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length - k + 1;
         int[] ans = new int[len];
-        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+        LinkedList<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < k; i++) {
-            queue.add(new int[]{nums[i], i});
+            while (!queue.isEmpty() && nums[i] >= nums[queue.getLast()]) {
+                queue.removeLast();
+            }
+            queue.addLast(i);
         }
 
-        ans[0] = queue.peek()[0];
+        ans[0] = nums[queue.getFirst()];
 
         for (int i = k; i < nums.length; i++) {
-            queue.add(new int[]{nums[i], i});
-            int peek;
-            while (queue.peek()[1] <= i - k) {
-                queue.poll();
+            while (!queue.isEmpty() && nums[i] >= nums[queue.getLast()]) {
+                queue.removeLast();
             }
-            ans[i - k + 1] = queue.peek()[0];
+            queue.addLast(i);
+
+            while (queue.getFirst() < i - k + 1) {
+                queue.removeFirst();
+            }
+            ans[i - k + 1] = nums[queue.getFirst()];
         }
 
         return ans;
