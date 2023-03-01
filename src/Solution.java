de49b886cae459554af26d686236f27d7a0d70d0
yanglongqi <yanglongqi@xiaomi.com>
2023-03-01 19:11:52 +0800

    [240]搜索二维矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,22 @@
-import java.util.Arrays;
-
 class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        int len = matrix.length;
+        int m = matrix.length;
         int n = matrix[0].length;
-        for (int[] ints : matrix) {
-            if (ints[n - 1] >= target && find(ints, target)) {
-                return true;
+        int x = 0, y = n - 1;
+        while (true) {
+            if (x >= m || y < 0) {
+                return false;
             }
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                x++;
+            } else {
+                y--;
+            }
+
         }
-
-        return false;
     }
 
-    private boolean find(int[] nums, int target) {
-        return Arrays.binarySearch(nums, target) >= 0;
-    }
 }
