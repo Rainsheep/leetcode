88157c3169eda24a8994df8367fc11fb8a6dd33b
yanglongqi <yanglongqi@xiaomi.com>
2023-03-01 19:01:05 +0800

    [240]搜索二维矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,19 @@
-import java.util.LinkedList;
+import java.util.Arrays;
 
 class Solution {
 
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int len = nums.length - k + 1;
-        int[] ans = new int[len];
-        LinkedList<Integer> queue = new LinkedList<>();
-
-        for (int i = 0; i < k; i++) {
-            while (!queue.isEmpty() && nums[i] >= nums[queue.getLast()]) {
-                queue.removeLast();
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int len = matrix.length;
+        for (int[] ints : matrix) {
+            if (find(ints, target)) {
+                return true;
             }
-            queue.addLast(i);
         }
 
-        ans[0] = nums[queue.getFirst()];
+        return false;
+    }
 
-        for (int i = k; i < nums.length; i++) {
-            while (!queue.isEmpty() && nums[i] >= nums[queue.getLast()]) {
-                queue.removeLast();
-            }
-            queue.addLast(i);
-
-            while (queue.getFirst() < i - k + 1) {
-                queue.removeFirst();
-            }
-            ans[i - k + 1] = nums[queue.getFirst()];
-        }
-
-        return ans;
-
+    private boolean find(int[] nums, int target) {
+        return Arrays.binarySearch(nums, target) >= 0;
     }
 }
