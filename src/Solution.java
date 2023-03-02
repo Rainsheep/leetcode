5ee06bf28f69a8654cf7c5b55e0d25e08cb7cd34
yanglongqi <yanglongqi@xiaomi.com>
2023-03-02 14:23:45 +0800

    [复习][253]会议室II

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 class Solution {
 
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int x = 0;
-        int y = n - 1;
-        while (true) {
-            if (x == m || y < 0) {
-                return false;
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        for (int[] interval : intervals) {
+            if (!queue.isEmpty() && interval[0] >= queue.peek()[1]) {
+                queue.poll();
             }
-            if (matrix[x][y] == target) {
-                return true;
-            } else if (matrix[x][y] > target) {
-                y--;
-            } else {
-                x++;
-            }
+            queue.add(interval);
         }
+        return queue.size();
     }
 
+    public static void main(String[] args) {
+        int res = new Solution().minMeetingRooms(new int[][]{{0, 30}, {5, 10}, {15, 20}});
+        System.out.println(res == 2);
+        res = new Solution().minMeetingRooms(new int[][]{{7, 10}, {2, 4}});
+        System.out.println(res == 1);
+    }
 }
