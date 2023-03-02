8e1dee09a91542078700bf342797c8abbb485d77
yanglongqi <yanglongqi@xiaomi.com>
2023-03-02 14:09:38 +0800

    [复习][239]滑动窗口最大值

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,32 @@
+import java.util.LinkedList;
+
 class Solution {
 
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        ans[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            ans[i] = nums[i] * ans[i - 1];
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            while (!queue.isEmpty() && nums[i] >= nums[queue.peekLast()]) {
+                queue.removeLast();
+            }
+            queue.add(i);
+        }
+        res[0] = nums[queue.peek()];
+        for (int i = k; i < len; i++) {
+            if (queue.peek() < i - k + 1) {
+                queue.pop();
+            }
+            while (!queue.isEmpty() && nums[i] >= nums[queue.peekLast()]) {
+                queue.removeLast();
+            }
+            queue.add(i);
+
+            res[i - k + 1] = nums[queue.peek()];
         }
 
-        int suf = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] = (i == 0 ? 1 : ans[i - 1]) * suf;
-            suf *= nums[i];
-        }
-        return ans;
+        return res;
+
+
     }
 }
