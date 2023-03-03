ac67f1336dff1eaa5dd7c40165c7a28b71288df0
yanglongqi <yanglongqi@xiaomi.com>
2023-03-03 15:27:04 +0800

    [301]删除无效的括号

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,71 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 class Solution {
 
-    public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            if (list.isEmpty() || num > list.get(list.size() - 1)) {
-                list.add(num);
-            } else {
-                list.set(findIndex(list, num), num);
+    HashSet<String> ans = new HashSet<>();
+
+    public List<String> removeInvalidParentheses(String s) {
+
+        int lremove = 0;
+        int rremove = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lremove++;
+            } else if (s.charAt(i) == ')') {
+                if (lremove > 0) {
+                    lremove--;
+                } else {
+                    rremove++;
+                }
             }
         }
+        tryDelete(s, 0, lremove, rremove);
 
-        return list.size();
+        return new ArrayList<>(ans);
     }
 
-    private int findIndex(List<Integer> list, int target) {
-        int index = Collections.binarySearch(list, target);
-        return index >= 0 ? index : -index - 1;
+    private void tryDelete(String s, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isVail(s)) {
+                ans.add(s);
+            }
+            return;
+        }
+
+        if (s.length() - start < lremove + rremove) {
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (i != start && s.charAt(i) == s.charAt(i - 1)) {
+                continue;
+            }
+
+            if (s.charAt(i) == '(' && lremove > 0) {
+                tryDelete(s.substring(0, i) + s.substring(i + 1), i, lremove - 1, rremove);
+            }
+
+            if (s.charAt(i) == ')' && rremove > 0) {
+                tryDelete(s.substring(0, i) + s.substring(i + 1), i, lremove, rremove - 1);
+            }
+        }
+    }
+
+
+    private boolean isVail(String s) {
+        int lNum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lNum++;
+            } else if (s.charAt(i) == ')') {
+                if (lNum == 0) {
+                    return false;
+                }
+                lNum--;
+            }
+        }
+        return lNum == 0;
     }
 }
