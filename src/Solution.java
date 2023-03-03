1b59732f52689c88db1c97920784c6bd4c6e59dd
yanglongqi <yanglongqi@xiaomi.com>
2023-03-03 16:20:49 +0800

    [123]买卖股票的最佳时机 III

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,17 @@
 class Solution {
 
     public int maxProfit(int[] prices) {
-        int ans = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                ans += prices[i] - prices[i - 1];
-            }
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+        for (int i = 0; i < prices.length; i++) {
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, prices[i] + buy1);
+
+            buy2 = Math.max(buy2, -prices[i] + sell1);
+            sell2 = Math.max(sell2, prices[i] + buy2);
         }
-        return ans;
+        return sell2;
     }
 }
