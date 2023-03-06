ddd1251c3c1b7e6f0c1bec748b83bf8b84ad39c1
yanglongqi <yanglongqi@xiaomi.com>
2023-03-06 15:51:29 +0800

    [复习][301]删除无效的括号

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,75 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 class Solution {
 
-    public int maxProfit(int k, int[] prices) {
-        int[] buys = new int[k];
-        int[] sells = new int[k];
-        Arrays.fill(buys, -prices[0]);
+    HashSet<String> ans = new HashSet<>();
 
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = 0; j < k; j++) {
-                buys[j] = Math.max(buys[j], -prices[i] + (j > 0 ? sells[j - 1] : 0));
-                sells[j] = Math.max(sells[j], prices[i] + buys[j]);
+    public List<String> removeInvalidParentheses(String s) {
+        int lremove = 0;
+        int rremove = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lremove++;
+            } else if (s.charAt(i) == ')') {
+                if (lremove == 0) {
+                    rremove++;
+                } else {
+                    lremove--;
+                }
             }
         }
-        return sells[k - 1];
+
+        tryDelete(s, lremove, rremove);
+        return new ArrayList<>(ans);
     }
+
+    private void tryDelete(String s, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            boolean vail = isVail(s);
+            if (vail) {
+                ans.add(s);
+            } else {
+                return;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0 && s.charAt(i) == s.charAt(i - 1)) {
+                continue;
+            }
+            if (s.length() - i < lremove + rremove) {
+                continue;
+            }
+
+            if (s.charAt(i) == '(') {
+                if (lremove > 0) {
+                    tryDelete(s.substring(0, i) + s.substring(i + 1), lremove - 1, rremove);
+                }
+            } else if (s.charAt(i) == ')') {
+                if (rremove > 0) {
+                    tryDelete(s.substring(0, i) + s.substring(i + 1), lremove, rremove - 1);
+                }
+            }
+        }
+    }
+
+    private boolean isVail(String s) {
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                num++;
+            } else if (s.charAt(i) == ')') {
+                if (num == 0) {
+                    return false;
+                } else {
+                    num--;
+                }
+            }
+        }
+        return num == 0;
+    }
+
+
 }
