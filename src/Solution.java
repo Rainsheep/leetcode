dccc0da227393b1986ac55712b61091465f16ef8
yanglongqi <yanglongqi@xiaomi.com>
2023-03-07 15:19:16 +0800

    [312]戳气球

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,22 @@
-import java.util.Arrays;
-
 class Solution {
 
-    int[] val;
-    int n;
-    int[][] cache;
-
     public int maxCoins(int[] nums) {
-        this.n = nums.length + 2;
-        val = new int[n];
+        int n = nums.length + 2;
+        int[] val = new int[n];
         System.arraycopy(nums, 0, val, 1, nums.length);
         val[0] = val[n - 1] = 1;
-
-        this.cache = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(cache[i], -1);
+        int[][] dp = new int[n][n];
+        for (int i = n - 3; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += dp[i][k] + dp[k][j];
+                    dp[i][j] = Math.max(dp[i][j], sum);
+                }
+            }
         }
-        return dfs(0, n - 1);
+
+        return dp[0][n - 1];
     }
 
-    private int dfs(int left, int right) {
-        if (left >= right - 1) {
-            return 0;
-        }
-        if (cache[left][right] != -1) {
-            return cache[left][right];
-        }
-
-        for (int i = left + 1; i < right; i++) {
-            int sum = val[left] * val[i] * val[right];
-            sum += dfs(left, i) + dfs(i, right);
-            cache[left][right] = Math.max(cache[left][right], sum);
-        }
-        return cache[left][right];
-    }
 }
