c233ae7d41b8617ba95695874a3d816ba25623de
yanglongqi <yanglongqi@xiaomi.com>
2023-03-07 14:08:54 +0800

    [714]买卖股票的最佳时机含手续费

--- src/Solution.java
+++ src/Solution.java
@@ -1,72 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
 
-    HashSet<String> ans = new HashSet<>();
-
-    public List<String> removeInvalidParentheses(String s) {
-        int lremove = 0;
-        int rremove = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                lremove++;
-            } else if (s.charAt(i) == ')') {
-                if (lremove == 0) {
-                    rremove++;
-                } else {
-                    lremove--;
-                }
-            }
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int buy = -prices[0];
+        int sell = 0;
+        for (int price : prices) {
+            int t = sell;
+            sell = Math.max(sell, buy + price - fee);
+            buy = Math.max(buy, t - price);
         }
-
-        tryDelete(s, 0, lremove, rremove);
-        return new ArrayList<>(ans);
+        return sell;
     }
-
-    private void tryDelete(String s, int start, int lremove, int rremove) {
-        if (lremove == 0 && rremove == 0) {
-            if (isVail(s)) {
-                ans.add(s);
-            } else {
-                return;
-            }
-        }
-
-        for (int i = start; i < s.length(); i++) {
-            if (i != 0 && s.charAt(i) == s.charAt(i - 1)) {
-                continue;
-            }
-            if (s.charAt(i) == '(') {
-                if (lremove > 0) {
-                    tryDelete(s.substring(0, i) + s.substring(i + 1), i, lremove - 1, rremove);
-                }
-            } else if (s.charAt(i) == ')') {
-                if (rremove > 0) {
-                    tryDelete(s.substring(0, i) + s.substring(i + 1), i, lremove, rremove - 1);
-                }
-
-            }
-        }
-    }
-
-
-    private boolean isVail(String s) {
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                num++;
-            } else if (s.charAt(i) == ')') {
-                if (num == 0) {
-                    return false;
-                } else {
-                    num--;
-                }
-            }
-        }
-        return num == 0;
-    }
-
-
 }
