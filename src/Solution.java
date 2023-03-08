f0b6f1c9f085625b0556d21f7ee45716e7d07416
Rainsheep <1045674971@qq.com>
2023-03-08 19:57:29 +0800

    [复习][312]戳气球

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
 
-    int space;
-    int[] dp;
-
-    public int coinChange(int[] coins, int amount) {
-        this.space = amount;
-        dp = new int[space + 1];
-        int defaultMax = Integer.MAX_VALUE - 1;
-        Arrays.fill(dp, defaultMax);
-        dp[0] = 0;
-        for (int coin : coins) {
-            completePack(coin, 1);
+    public int maxCoins(int[] nums) {
+        int n = nums.length + 2;
+        int[] val = new int[n];
+        System.arraycopy(nums, 0, val, 1, nums.length);
+        val[0] = val[n - 1] = 1;
+        int[][] dp = new int[n][n];
+        for (int i = n - 3; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int t = val[i] * val[k] * val[j];
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + t);
+                }
+            }
         }
-        return dp[space] == defaultMax ? -1 : dp[space];
-    }
+        return dp[0][n - 1];
 
-    private void completePack(int weight, int value) {
-        for (int i = weight; i <= space; i++) {
-            dp[i] = Math.min(dp[i], dp[i - weight] + value);
-        }
     }
 }
