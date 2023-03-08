2592783df03bd01f74a6b45bc4bafdee741a0a26
Rainsheep <1045674971@qq.com>
2023-03-08 20:17:47 +0800

    [198]打家劫舍

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,12 @@
 class Solution {
 
-    public int maxCoins(int[] nums) {
-        int n = nums.length + 2;
-        int[] val = new int[n];
-        System.arraycopy(nums, 0, val, 1, nums.length);
-        val[0] = val[n - 1] = 1;
-        int[][] dp = new int[n][n];
-        for (int i = n - 3; i >= 0; i--) {
-            for (int j = i + 2; j < n; j++) {
-                for (int k = i + 1; k < j; k++) {
-                    int t = val[i] * val[k] * val[j];
-                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + t);
-                }
-            }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = Math.max(i >= 1 ? dp[i - 1] : 0, (i >= 2 ? dp[i - 2] : 0) + nums[i]);
         }
-        return dp[0][n - 1];
+        return dp[n - 1];
 
     }
 }
