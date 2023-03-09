010aa2da3bdf4b9f53a5a3de2beeec1470ec86c2
yanglongqi <yanglongqi@xiaomi.com>
2023-03-09 19:34:11 +0800

    [复习][309]最佳买卖股票时机含冷冻期

--- src/Solution.java
+++ src/Solution.java
@@ -2,12 +2,13 @@ class Solution {
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        dp[0][0] = -prices[0];
+        int[] buys = new int[n];
+        int[] sells = new int[n];
+        buys[0] = -prices[0];
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], -prices[i] + (i >= 2 ? dp[i - 2][1] : 0));
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            buys[i] = Math.max(buys[i - 1], (i >= 2 ? sells[i - 2] : 0) - prices[i]);
+            sells[i] = Math.max(sells[i - 1], buys[i - 1] + prices[i]);
         }
-        return dp[n - 1][1];
+        return sells[n - 1];
     }
 }
