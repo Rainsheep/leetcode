ff6387da01f5301128bbd022e309bf320b984d37
yanglongqi <yanglongqi@xiaomi.com>
2023-03-09 19:56:44 +0800

    [338]比特位计数

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,12 @@
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
+    public int[] countBits(int num) {
+        int[] nums = new int[num + 1];
 
-    public int rob(TreeNode root) {
-        int[] res = dfs(root);
-        return Math.max(res[0], res[1]);
-    }
-
-    private int[] dfs(TreeNode root) {
-        if (root == null) {
-            return new int[]{0, 0};
+        for (int i = 1; i < num + 1; i++) {
+            nums[i] = nums[i >> 1] + (i & 1);
         }
-        int[] left = dfs(root.left);
-        int[] right = dfs(root.right);
-        int[] res = new int[2];
-        res[0] = left[1] + right[1] + root.val;
-        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        return res;
+
+        return nums;
+
     }
 }
