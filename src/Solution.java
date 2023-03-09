ed9d9673b94ee8aaf4a206743081642847dbf813
yanglongqi <yanglongqi@xiaomi.com>
2023-03-09 19:46:12 +0800

    [复习][337]打家劫舍 III

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,39 @@
-class Solution {
+class TreeNode {
 
-    public int maxCoins(int[] nums) {
-        int n = nums.length + 2;
-        int[] val = new int[n];
-        System.arraycopy(nums, 0, val, 1, nums.length);
-        val[0] = val[n - 1] = 1;
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        int[][] dp = new int[n][n];
-        for (int i = n - 3; i >= 0; i--) {
-            for (int j = i + 2; j < n; j++) {
-                for (int k = i + 1; k < j; k++) {
-                    int t = val[i] * val[j] * val[k];
-                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + t);
-                }
-            }
-        }
-        return dp[0][n - 1];
+    TreeNode() {
     }
 
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + root.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
 }
