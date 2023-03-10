a936551c08e058034b218a6951a85229825c2fba
yanglongqi <yanglongqi@xiaomi.com>
2023-03-10 16:10:00 +0800

    [347]前 K 个高频元素

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,30 @@
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 class Solution {
 
-    public int[] countBits(int num) {
-        int[] res = new int[num + 1];
-        for (int i = 0; i <= num; i++) {
-            res[i] = res[i << 1] + (i & 1);
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return res;
+
+        ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> list = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            list.add(new AbstractMap.SimpleEntry<>(key, map.get(key)));
+        }
+
+        PriorityQueue<AbstractMap.SimpleEntry<Integer, Integer>> queue = new PriorityQueue<>(
+                (o1, o2) -> o2.getValue() - o1.getValue());
+
+        queue.addAll(list);
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll().getKey();
+        }
+        return ans;
     }
 }
