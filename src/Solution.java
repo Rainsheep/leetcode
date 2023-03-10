fbd5dff8ef00768671848115a461926587367127
yanglongqi <yanglongqi@xiaomi.com>
2023-03-10 17:18:51 +0800

    [394]字符串解码

--- src/Solution.java
+++ src/Solution.java
@@ -1,58 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Random;
-
 class Solution {
 
-    int[] ans;
-    int ansSize;
-
-    public int[] topKFrequent(int[] nums, int k) {
-        ansSize = 0;
-        ans = new int[k];
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+    public String decodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isNumChar(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            } else if (s.charAt(i) == '[') {
+                int closeIndex = findCloseIndex(s, i);
+                String sub = s.substring(i + 1, closeIndex);
+                String ss = decodeString(sub);
+                for (int k = 0; k < num; k++) {
+                    sb.append(ss);
+                }
+                i = closeIndex;
+                num = 0;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
-
-        ArrayList<int[]> list = new ArrayList<>();
-
-        for (Entry<Integer, Integer> entry : map.entrySet()) {
-            list.add(new int[]{entry.getKey(), entry.getValue()});
-        }
-        qsort(list, 0, list.size() - 1, k);
-
-        return ans;
+        return sb.toString();
     }
 
-    private void qsort(ArrayList<int[]> list, int start, int end, int k) {
-        int pickedIndex = new Random().nextInt(end - start + 1) + start;
-        int pickedVal = list.get(pickedIndex)[1];
-        int index = start;
-        for (int i = start; i <= end; i++) {
-            if (list.get(i)[1] >= pickedVal) {
-                Collections.swap(list, i, index++);
-            }
-        }
-
-        if (k == index - start) {
-            for (int i = start; i < index; i++) {
-                ans[ansSize++] = list.get(i)[0];
-            }
-        } else if (k < index - start) {
-            qsort(list, start, index - 1, k);
-        } else {
-            for (int i = start; i < index; i++) {
-                ans[ansSize++] = list.get(i)[0];
-            }
-            qsort(list, index, end, k - (index - start));
-        }
+    private boolean isNumChar(char c) {
+        return c >= '0' && c <= '9';
     }
 
-    public static void main(String[] args) {
-        new Solution().topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+    private int findCloseIndex(String s, int begin) {
+        int left = 1;
+        for (int i = begin + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '[') {
+                left++;
+            } else if (s.charAt(i) == ']') {
+                left--;
+            }
+            if (left == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
