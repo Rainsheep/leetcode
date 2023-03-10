cbc78eca37fb82a09b001752ba3d52e6342ccef9
yanglongqi <yanglongqi@xiaomi.com>
2023-03-10 18:42:18 +0800

    [399]除法求值

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,61 @@
+import java.util.HashMap;
+import java.util.List;
+
 class Solution {
 
-    public String decodeString(String s) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (isNumChar(s.charAt(i))) {
-                num = num * 10 + s.charAt(i) - '0';
-            } else if (s.charAt(i) == '[') {
-                int closeIndex = findCloseIndex(s, i);
-                String sub = s.substring(i + 1, closeIndex);
-                String ss = decodeString(sub);
-                for (int k = 0; k < num; k++) {
-                    sb.append(ss);
+    HashMap<String, String> father = new HashMap<>();
+    HashMap<String, Double> multiple = new HashMap<>();
+
+    private void merge(String a, String b, double val) {
+        // a/b=val
+        String father1 = findFather(a);
+        String father2 = findFather(b);
+        if (!father1.equals(father2)) {
+            father.put(father1, father2);
+            multiple.put(father1, val * multiple.get(b) / multiple.get(a));
+        }
+    }
+
+    private String findFather(String s) {
+        String p = this.father.get(s);
+        if (p == null || s.equals(p)) {
+            return s;
+        }
+
+        String pp = findFather(p);
+        // 压缩的同时更新倍数
+        multiple.put(s, multiple.get(s) * multiple.get(p));
+        father.put(s, pp);
+        return pp;
+    }
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        int i = 0;
+        for (List<String> equation : equations) {
+            for (String s : equation) {
+                if (!father.containsKey(s)) {
+                    father.put(s, s);
+                    multiple.put(s, 1.0);
                 }
-                i = closeIndex;
-                num = 0;
+
+            }
+            merge(equation.get(0), equation.get(1), values[i++]);
+        }
+
+        double[] ans = new double[queries.size()];
+        i = 0;
+        for (List<String> query : queries) {
+            String a = findFather(query.get(0));
+            String b = findFather(query.get(1));
+
+            if (multiple.get(query.get(0)) == null) {
+                ans[i++] = -1.0;
             } else {
-                sb.append(s.charAt(i));
+                ans[i++] = a.equals(b) ? multiple.get(query.get(0)) / multiple.get(query.get(1)) : -1.0;
             }
-        }
-        return sb.toString();
-    }
 
-    private boolean isNumChar(char c) {
-        return c >= '0' && c <= '9';
-    }
-
-    private int findCloseIndex(String s, int begin) {
-        int left = 1;
-        for (int i = begin + 1; i < s.length(); i++) {
-            if (s.charAt(i) == '[') {
-                left++;
-            } else if (s.charAt(i) == ']') {
-                left--;
-            }
-            if (left == 0) {
-                return i;
-            }
         }
-        return -1;
+        return ans;
     }
 }
