edab14face3142e16546e9db7985213ad3a794bf
yanglongqi <yanglongqi@xiaomi.com>
2023-03-10 15:00:08 +0800

    [复习][338]比特位计数

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,10 @@
 class Solution {
+
     public int[] countBits(int num) {
-        int[] nums = new int[num + 1];
-
-        for (int i = 1; i < num + 1; i++) {
-            nums[i] = nums[i >> 1] + (i & 1);
+        int[] res = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            res[i] = res[i << 1] + (i & 1);
         }
-
-        return nums;
-
+        return res;
     }
 }
