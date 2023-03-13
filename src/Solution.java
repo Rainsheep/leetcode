56bbac02668bb5bf1a46dcc520e6227622f1c743
yanglongqi <yanglongqi@xiaomi.com>
2023-03-13 14:04:07 +0800

    [406]根据身高重建队列

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,25 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
 
     public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (o1, o2) -> o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]);
-        ArrayList<int[]> list = new ArrayList<>(Arrays.asList(people));
-
-        for (int i = 0; i < list.size(); i++) {
-            int t = list.get(i)[1];
-            int j = 0;
-            for (; j < i && t > 0; j++) {
-                if (list.get(j)[0] >= list.get(i)[0]) {
-                    t--;
+        Arrays.sort(people, (o1, o2) -> o1[0] != o2[0] ? o1[0] - o2[0] : o2[1] - o1[1]);
+        int[][] ans = new int[people.length][];
+        for (int[] person : people) {
+            int space = person[1] + 1;
+            for (int j = 0; j < people.length; ++j) {
+                if (ans[j] == null) {
+                    --space;
+                    if (space == 0) {
+                        ans[j] = person;
+                        break;
+                    }
                 }
+
             }
 
-            while (j < i && list.get(i)[0] > list.get(j)[0]) {
-                j++;
-            }
-            if (i != j) {
-                int[] remove = list.remove(i);
-                list.add(j, remove);
-            }
         }
 
-        int[][] ans = new int[list.size()][2];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = list.get(i);
-        }
         return ans;
     }
 }
