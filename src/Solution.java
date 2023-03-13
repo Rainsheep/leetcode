b695a5738c423e43fcbefd775c3a6ffd5c514590
yanglongqi <yanglongqi@xiaomi.com>
2023-03-13 13:26:36 +0800

    [406]根据身高重建队列

--- src/Solution.java
+++ src/Solution.java
@@ -1,69 +1,34 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
 
-    HashMap<String, String> father = new HashMap<>();
-    HashMap<String, Double> multi = new HashMap<>();
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]);
+        ArrayList<int[]> list = new ArrayList<>(Arrays.asList(people));
 
-    private void merge(String a, String b, double val) {
-        String fa = findFather(a);
-        String fb = findFather(b);
-        if (!fa.equals(fb)) {
-            father.put(fa, fb);
-            multi.put(fa, val * multi.get(b) / multi.get(a));
-        }
-    }
+        for (int i = 0; i < list.size(); i++) {
+            int t = list.get(i)[1];
+            int j = 0;
+            for (; j < i && t > 0; j++) {
+                if (list.get(j)[0] >= list.get(i)[0]) {
+                    t--;
+                }
+            }
 
-    private String findFather(String x) {
-        String fx = father.get(x);
-        if (fx == null) {
-            return null;
-        }
-
-        if (fx.equals(x)) {
-            return x;
-        }
-        String fatherfx = findFather(fx);
-        father.put(x, fatherfx);
-        multi.put(x, multi.get(x) * multi.get(fx));
-        return fatherfx;
-    }
-
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        int i = 0;
-        for (List<String> equation : equations) {
-            init(equation.get(0));
-            init(equation.get(1));
-            merge(equation.get(0), equation.get(1), values[i++]);
+            while (j < i && list.get(i)[0] > list.get(j)[0]) {
+                j++;
+            }
+            if (i != j) {
+                int[] remove = list.remove(i);
+                list.add(j, remove);
+            }
         }
 
-        // printMulti();
-
-        double[] ans = new double[queries.size()];
-        i = 0;
-        for (List<String> query : queries) {
-            String father1 = findFather(query.get(0));
-            String father2 = findFather(query.get(1));
-            // System.out.println(father1 + " " + father2);
-            ans[i++] = father1 != null && father1.equals(father2) ? multi.get(query.get(0)) / multi.get(
-                    query.get(1)) : -1.0;
+        int[][] ans = new int[list.size()][2];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = list.get(i);
         }
-
         return ans;
-    }
-
-    private void printMulti() {
-        for (Entry<String, Double> entry : multi.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-    }
-
-    private void init(String x) {
-        if (!father.containsKey(x)) {
-            father.put(x, x);
-            multi.put(x, 1.0);
-        }
     }
 }
