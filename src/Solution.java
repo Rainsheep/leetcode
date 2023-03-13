213c78ead6afe3b1e14785ac3c5a5c7791804299
yanglongqi <yanglongqi@xiaomi.com>
2023-03-13 11:32:18 +0800

    [复习][399]除法求值

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,69 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Random;
 
 class Solution {
 
-    int[] ans;
-    int ansLen;
-    HashMap<Integer, Integer> map;
+    HashMap<String, String> father = new HashMap<>();
+    HashMap<String, Double> multi = new HashMap<>();
 
-    public int[] topKFrequent(int[] nums, int k) {
-        map = new HashMap<>();
-        ansLen = 0;
-        ans = new int[k];
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+    private void merge(String a, String b, double val) {
+        String fa = findFather(a);
+        String fb = findFather(b);
+        if (!fa.equals(fb)) {
+            father.put(fa, fb);
+            multi.put(fa, val * multi.get(b) / multi.get(a));
+        }
+    }
+
+    private String findFather(String x) {
+        String fx = father.get(x);
+        if (fx == null) {
+            return null;
         }
 
-        ArrayList<Integer> list = new ArrayList<>(map.size());
+        if (fx.equals(x)) {
+            return x;
+        }
+        String fatherfx = findFather(fx);
+        father.put(x, fatherfx);
+        multi.put(x, multi.get(x) * multi.get(fx));
+        return fatherfx;
+    }
 
-        for (Entry<Integer, Integer> entry : map.entrySet()) {
-            list.add(entry.getKey());
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int i = 0;
+        for (List<String> equation : equations) {
+            init(equation.get(0));
+            init(equation.get(1));
+            merge(equation.get(0), equation.get(1), values[i++]);
         }
 
-        qsort(list, 0, list.size() - 1, k);
+        // printMulti();
+
+        double[] ans = new double[queries.size()];
+        i = 0;
+        for (List<String> query : queries) {
+            String father1 = findFather(query.get(0));
+            String father2 = findFather(query.get(1));
+            // System.out.println(father1 + " " + father2);
+            ans[i++] = father1 != null && father1.equals(father2) ? multi.get(query.get(0)) / multi.get(
+                    query.get(1)) : -1.0;
+        }
+
         return ans;
     }
 
-    private void qsort(List<Integer> list, int start, int end, int k) {
-        int pickIndex = new Random().nextInt(end - start + 1) + start;
-        int pickVal = list.get(pickIndex);
-        int index = start;
-        for (int i = start; i <= end; i++) {
-            if (map.get(list.get(i)) >= map.get(pickVal)) {
-                Collections.swap(list, i, index++);
-            }
-        }
-
-        if (k == index - start) {
-            addToAns(list, start, index);
-        } else if (index - start > k) {
-            qsort(list, start, index - 1, k);
-        } else {
-            addToAns(list, start, index);
-            qsort(list, index, end, k - (index - start));
+    private void printMulti() {
+        for (Entry<String, Double> entry : multi.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 
-    private void addToAns(List<Integer> list, int start, int end) {
-        for (int i = start; i < end; i++) {
-            ans[ansLen++] = list.get(i);
+    private void init(String x) {
+        if (!father.containsKey(x)) {
+            father.put(x, x);
+            multi.put(x, 1.0);
         }
     }
 }
