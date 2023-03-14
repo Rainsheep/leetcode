937fdbd3de8bbe7ae1d136b5bb36f03059b6db5b
yanglongqi <yanglongqi@xiaomi.com>
2023-03-14 16:30:03 +0800

    [448]找到所有数组中消失的数字

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,27 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 class Solution {
 
-    public List<Integer> findAnagrams(String s, String p) {
-        int pLen = p.length();
-        int sLen = s.length();
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
         ArrayList<Integer> ans = new ArrayList<>();
-        if (pLen > sLen) {
-            return ans;
-        }
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < pLen; i++) {
-            char c = p.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) - 1);
-        }
-
-        for (int i = 0; i < pLen; i++) {
-            char c = s.charAt(i);
-            Integer num = map.getOrDefault(c, 0);
-            if (num == -1) {
-                map.remove(c);
-            } else {
-                map.put(c, num + 1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                ans.add(i + 1);
             }
         }
-        if (map.isEmpty()) {
-            ans.add(0);
-        }
-
-        for (int i = pLen; i < sLen; i++) {
-            char c = s.charAt(i);
-            Integer num = map.getOrDefault(c, 0);
-            if (num == -1) {
-                map.remove(c);
-            } else {
-                map.put(c, num + 1);
-            }
-
-            char oldC = s.charAt(i - pLen);
-            num = map.getOrDefault(oldC, 0);
-            if (num == 1) {
-                map.remove(oldC);
-            } else {
-                map.put(oldC, num - 1);
-            }
-
-            if (map.isEmpty()) {
-                ans.add(i - pLen + 1);
-            }
-        }
-
         return ans;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
