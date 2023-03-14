45d0dfffef55f93fdb2c9367d010c5bee70e796d
yanglongqi <yanglongqi@xiaomi.com>
2023-03-14 17:05:04 +0800

    [494]目标和

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,26 @@
 class Solution {
 
-    public int hammingDistance(int x, int y) {
-        int z = x ^ y;
-        int ans = 0;
-        while (z > 0) {
-            ans += z & 1;
-            z >>= 1;
-        }
+    int target;
+    int ans;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        this.target = target;
+        ans = 0;
+        dfs(nums, 0, 0);
         return ans;
+    }
+
+    private void dfs(int[] nums, int index, int sum) {
+        if (index == nums.length) {
+            if (sum == target) {
+                ans++;
+            }
+            return;
+        }
+
+        int sum1 = sum + nums[index];
+        int sum2 = sum - nums[index];
+        dfs(nums, index + 1, sum1);
+        dfs(nums, index + 1, sum2);
     }
 }
