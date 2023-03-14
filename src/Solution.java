4647d5686d72d01812b608bc72dd1d68b8d132f8
yanglongqi <yanglongqi@xiaomi.com>
2023-03-14 16:21:16 +0800

    [438]找到字符串中所有字母异位词

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,57 @@
 import java.util.ArrayList;
-
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+import java.util.HashMap;
+import java.util.List;
 
 class Solution {
 
-    int ans = 0;
-    int targetSum;
-
-    public int pathSum(TreeNode root, int targetSum) {
-        this.targetSum = targetSum;
-        dfs(root, new ArrayList<>());
-        return ans;
-    }
-
-    private void dfs(TreeNode root, ArrayList<Integer> list) {
-        if (root == null) {
-            return;
+    public List<Integer> findAnagrams(String s, String p) {
+        int pLen = p.length();
+        int sLen = s.length();
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (pLen > sLen) {
+            return ans;
         }
-        list.add(root.val);
-        long sum = 0;
-        for (int i = list.size() - 1; i >= 0; i--) {
-            sum += list.get(i);
-            if (sum == targetSum) {
-                ans++;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < pLen; i++) {
+            char c = p.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for (int i = 0; i < pLen; i++) {
+            char c = s.charAt(i);
+            Integer num = map.getOrDefault(c, 0);
+            if (num == -1) {
+                map.remove(c);
+            } else {
+                map.put(c, num + 1);
             }
         }
-        ArrayList<Integer> rightList = new ArrayList<>(list);
-        dfs(root.left, list);
-        dfs(root.right, rightList);
+        if (map.isEmpty()) {
+            ans.add(0);
+        }
+
+        for (int i = pLen; i < sLen; i++) {
+            char c = s.charAt(i);
+            Integer num = map.getOrDefault(c, 0);
+            if (num == -1) {
+                map.remove(c);
+            } else {
+                map.put(c, num + 1);
+            }
+
+            char oldC = s.charAt(i - pLen);
+            num = map.getOrDefault(oldC, 0);
+            if (num == 1) {
+                map.remove(oldC);
+            } else {
+                map.put(oldC, num - 1);
+            }
+
+            if (map.isEmpty()) {
+                ans.add(i - pLen + 1);
+            }
+        }
+
+        return ans;
     }
 }
