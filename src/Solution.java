b221702d0cf19984a8b4a6eeea64b38511f83c32
yanglongqi <yanglongqi@xiaomi.com>
2023-03-14 16:35:08 +0800

    [461]汉明距离

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
-                swap(nums, i, nums[i] - 1);
-            }
-        }
-
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                ans.add(i + 1);
-            }
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int ans = 0;
+        while (z > 0) {
+            ans += z & 1;
+            z >>= 1;
         }
         return ans;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
     }
 }
