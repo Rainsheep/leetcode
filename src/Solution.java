73ffb3be5fcaddf511cbd0439d08ec2b03698775
yanglongqi <yanglongqi@xiaomi.com>
2023-03-16 12:40:38 +0800

    [494]目标和

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,24 @@
 class Solution {
 
-    int target;
-    int ans;
-
     public int findTargetSumWays(int[] nums, int target) {
-        this.target = target;
-        ans = 0;
-        dfs(nums, 0, 0);
-        return ans;
-    }
-
-    private void dfs(int[] nums, int index, int sum) {
-        if (index == nums.length) {
-            if (sum == target) {
-                ans++;
-            }
-            return;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
+        sum -= target;
+        if (sum < 0 || sum % 2 != 0) {
+            return 0;
+        }
+        sum /= 2;
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
 
-        int sum1 = sum + nums[index];
-        int sum2 = sum - nums[index];
-        dfs(nums, index + 1, sum1);
-        dfs(nums, index + 1, sum2);
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[sum];
     }
+
 }
