6c277e1e3505ca996a00c68689e926c0fcf11f06
yanglongqi <yanglongqi@xiaomi.com>
2023-03-17 14:31:00 +0800

    [560]和为 K 的子数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,18 @@
+import java.util.HashMap;
+
 class Solution {
 
     public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
         int ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j >= 0; j--) {
-                sum += nums[j];
-                ans += (sum == k ? 1 : 0);
-            }
+        for (int num : nums) {
+            sum += num;
+            int left = sum - k;
+            ans += map.getOrDefault(left, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return ans;
-
     }
 }
