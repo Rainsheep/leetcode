3a8a04a873827fae800d8cd89ad9693d679b4d44
yanglongqi <yanglongqi@xiaomi.com>
2023-03-17 16:11:44 +0800

    [581]最短无序连续子数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,53 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
 
-    public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        int sum = 0;
-        int ans = 0;
-        for (int num : nums) {
-            sum += num;
-            int left = sum - k;
-            ans += map.getOrDefault(left, 0);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+    public int findUnsortedSubarray(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < nums.length - 1 && nums[left] <= nums[left + 1]) {
+            left++;
         }
-        return ans;
+
+        while (right >= 1 && nums[right] >= nums[right - 1]) {
+            right--;
+        }
+
+        if (left >= right) {
+            return 0;
+        }
+
+        int[] flag = find(nums, left, right + 1);
+        int leftIndex = Arrays.binarySearch(nums, 0, left + 1, flag[0]);
+        int rightIndex = Arrays.binarySearch(nums, right, nums.length, flag[1]);
+
+        if (leftIndex < 0) {
+            leftIndex = -leftIndex - 1;
+        } else {
+            while (nums[leftIndex] == nums[leftIndex + 1]) {
+                leftIndex++;
+            }
+            leftIndex++;
+        }
+
+        rightIndex = rightIndex < 0 ? -rightIndex - 1 : rightIndex;
+
+        return rightIndex - leftIndex;
+    }
+
+    private int[] find(int[] nums, int i, int j) {
+        int min = nums[i];
+        int max = nums[i];
+
+        for (int k = i; k < j; k++) {
+            max = Math.max(nums[k], max);
+            min = Math.min(nums[k], min);
+        }
+        return new int[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findUnsortedSubarray(new int[]{2, 3, 3, 2, 4}));
+
     }
 }
