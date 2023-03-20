b6ca8ca45a5f19b1eb1a44903abefadc188ddc17
yanglongqi <yanglongqi@xiaomi.com>
2023-03-20 16:06:07 +0800

    [621]任务调度器

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,65 @@
-class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+import java.util.HashMap;
 
 class Solution {
 
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null) {
-            return root1 == null ? root2 : root1;
+    HashMap<Character, int[]> map = new HashMap<>();
+
+    public int leastInterval(char[] tasks, int n) {
+
+        for (char task : tasks) {
+            int[] taskEntry = map.getOrDefault(task, new int[2]);
+            taskEntry[1]++;
+            map.put(task, taskEntry);
         }
-        root1.val += root2.val;
-        TreeNode left = mergeTrees(root1.left, root2.left);
-        TreeNode right = mergeTrees(root1.right, root2.right);
-        root1.left = left;
-        root1.right = right;
-        return root1;
+
+        int currentTime = 0;
+        Character c;
+        while ((c = getNextTask(currentTime)) != null) {
+            int[] entry = map.get(c);
+            if (entry[0] > currentTime) {
+                currentTime = entry[0];
+            }
+            currentTime++;
+
+            entry[0] = currentTime + n;
+            if (entry[1] == 1) {
+                map.remove(c);
+            } else {
+                entry[1] -= 1;
+            }
+        }
+        return currentTime;
+    }
+
+    private Character getNextTask(int currentTime) {
+        if (map.isEmpty()) {
+            return null;
+        }
+
+        Character res = null;
+        for (Character character : map.keySet()) {
+            if (res == null) {
+                res = character;
+                continue;
+            }
+            int[] resEntry = map.get(res);
+            int[] entry = map.get(character);
+
+            if (entry[0] > currentTime && resEntry[0] <= currentTime) {
+                continue;
+            }
+
+            if (entry[0] <= currentTime && resEntry[0] > currentTime) {
+                res = character;
+                continue;
+            }
+
+            if (entry[0] > currentTime && resEntry[0] > currentTime && entry[0] != resEntry[0]) {
+                res = entry[0] < resEntry[0] ? character : res;
+            }
+
+            res = entry[1] > resEntry[1] ? character : res;
+        }
+        return res;
     }
 }
