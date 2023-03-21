bfffa6dc9285d7854dccd8834d4fd232e0633d16
yanglongqi <yanglongqi@xiaomi.com>
2023-03-21 17:38:40 +0800

    [647]回文子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,46 @@
-import java.util.HashMap;
-
 class Solution {
 
-    HashMap<Character, int[]> map = new HashMap<>();
+    int[][] flag;
 
-    public int leastInterval(char[] tasks, int n) {
-        int N = 26;
-        int count = 0;
-        int max = 0;
-        char[] nums = new char[N];
-        for (char task : tasks) {
-            int index = task - 'A';
-            nums[index]++;
-
-            if (nums[index] > max) {
-                max = nums[index];
-                count = 1;
-            } else if (nums[index] == max) {
-                count++;
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int ans = 0;
+        flag = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (isVail(s, j, i)) {
+                    ans++;
+                }
             }
         }
-
-        return Math.max((max - 1) * (n + 1) + count, tasks.length);
+        return ans;
     }
 
+    private boolean isVail(String s, int begin, int end) {
+        if (flag[begin][end] == 1) {
+            return true;
+        }
+        if (flag[begin][end] == 2) {
+            return false;
+        }
+
+        if (end <= begin) {
+            if (begin == end) {
+                flag[begin][end] = 1;
+            }
+            return true;
+        }
+
+        boolean b = s.charAt(begin) == s.charAt(end);
+        if (!b) {
+            flag[begin][end] = 2;
+            return false;
+        }
+        flag[begin][end] = isVail(s, begin + 1, end - 1) ? 1 : 2;
+        return flag[begin][end] == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countSubstrings("ab"));
+    }
 }
