cba9dc4a9f50557b42e7bca551806c86f1401f94
yanglongqi <yanglongqi@xiaomi.com>
2023-03-22 13:49:09 +0800

    [复习][647]回文子串

--- src/Solution.java
+++ src/Solution.java
@@ -2,64 +2,42 @@ import java.util.ArrayList;
 
 class Solution {
 
-    public String longestPalindrome(String s) {
-        int start = 0, end = -1;
+    public int countSubstrings(String s) {
+        int ans = 0;
         s = wrapperStr(s);
+        int cid = -1, right = -1;
         ArrayList<Integer> list = new ArrayList<>();
-        int right = -1, cid = -1;
-        for (int i = 0; i < s.length(); i++) {
-            int currentArmLen;
-            if (i <= right) {
-                int armLen = Math.min(list.get(2 * cid - i), right - i);
-                currentArmLen = expand(s, i - armLen, i + armLen);
-            } else {
-                currentArmLen = expand(s, i, i);
+        list.add(1);
+        for (int i = 1; i < s.length() - 1; i++) {
+            int armLen = i <= right ? Math.min(list.get(2 * cid - i), right - i + 1) : 1;
+            while (s.charAt(i + armLen) == s.charAt(i - armLen)) {
+                armLen++;
             }
-            list.add(currentArmLen);
+            list.add(armLen);
 
-            if (i + currentArmLen > right) {
+            if (i + armLen - 1 > right) {
                 cid = i;
-                right = i + currentArmLen;
+                right = i + armLen - 1;
             }
-            if (currentArmLen * 2 + 1 > end - start) {
-                start = i - currentArmLen;
-                end = i + currentArmLen;
-            }
+
+            // System.out.println(i + " " + armLen / 2);
+            ans += armLen / 2;
         }
 
-        return getAns(s, start, end);
-
+        return ans;
     }
 
-    private String wrapperStr(String s) {
-        StringBuilder sb = new StringBuilder("#");
+    public String wrapperStr(String s) {
+        StringBuilder sb = new StringBuilder("^#");
         for (int i = 0; i < s.length(); i++) {
             sb.append(s.charAt(i));
             sb.append("#");
         }
+        sb.append("$");
         return sb.toString();
     }
 
-    private String getAns(String s, int start, int end) {
-        StringBuilder ans = new StringBuilder();
-        for (int i = start; i <= end; ++i) {
-            if (s.charAt(i) != '#') {
-                ans.append(s.charAt(i));
-            }
-        }
-        return ans.toString();
-    }
-
-    public int expand(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return (right - left - 2) / 2;
-    }
-
     public static void main(String[] args) {
-        new Solution().longestPalindrome("babad");
+        System.out.println(new Solution().countSubstrings("aba"));
     }
-
 }
