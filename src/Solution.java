8d0ed7b264b4636c1a6921d4d4c8b27807d13cfe
yanglongqi <yanglongqi@xiaomi.com>
2023-03-27 16:54:03 +0800

    [复习][5]最长回文子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,62 @@
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 class Solution {
 
-    public int[] dailyTemperatures(int[] temperatures) {
-        int len = temperatures.length;
-        int[] ans = new int[len];
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+    public String longestPalindrome(String s) {
+        s = dealStr(s);
+        int len = s.length();
+        ArrayList<Integer> list = new ArrayList<>(len);
+        int cid = -1, right = -1;
+        int armLen;
+        int begin = 0, end = 0, maxArmLen = 0;
         for (int i = 0; i < len; i++) {
-            if (stack.isEmpty() || temperatures[stack.peek()] >= temperatures[i]) {
-                stack.push(i);
+            if (i <= right) {
+                armLen = Math.min(list.get(2 * cid - i), right - i + 1);
             } else {
-                while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
-                    Integer index = stack.poll();
-                    ans[index] = i - index;
-                }
-                stack.push(i);
+                armLen = 1;
+            }
+            armLen = expend(s, i - armLen + 1, i + armLen - 1);
+            list.add(armLen);
+
+            if (i + armLen - 1 > right) {
+                right = i + armLen - 1;
+                cid = i;
+            }
+
+            if (armLen > maxArmLen) {
+                maxArmLen = armLen;
+                begin = i - armLen + 1;
+                end = i + armLen;
+            }
+
+        }
+
+        // System.out.println(list);
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = begin; i < end; i++) {
+            if (s.charAt(i) != '#') {
+                ans.append(s.charAt(i));
             }
         }
 
-        return ans;
+        return ans.toString();
+    }
+
+    private String dealStr(String s) {
+        StringBuilder sb = new StringBuilder("#");
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            sb.append("#");
+        }
+        return sb.toString();
+    }
+
+    private int expend(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return (right - left - 1) / 2 + 1;
     }
 }
