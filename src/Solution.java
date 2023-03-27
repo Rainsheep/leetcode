65584bd508de9e8816289d76717d6683bdcdbaa4
yanglongqi <yanglongqi@xiaomi.com>
2023-03-27 17:30:24 +0800

    [复习][11]盛最多水的容器

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,18 @@
 class Solution {
 
-    public boolean isMatch(String s, String p) {
-        int sLen = s.length();
-        int pLen = p.length();
-        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
-
-        dp[0][0] = true;
-        for (int i = 0; i <= sLen; i++) {
-            for (int j = 1; j <= pLen; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] =
-                            dp[i][j - 2] || (i >= 1 && dp[i - 1][j] && isMatchChar(s.charAt(i - 1), p.charAt(j - 2)));
-                } else {
-                    dp[i][j] = i != 0 && (dp[i - 1][j - 1] && isMatchChar(s.charAt(i - 1), p.charAt(j - 1)));
-                }
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int ans = 0;
+        while (left < right) {
+            ans = Math.max((right - left) * Math.min(height[left], height[right]), ans);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
+
         }
-        return dp[sLen][pLen];
-    }
-
-    private boolean isMatchChar(char a, char b) {
-        return b == '.' || a == b;
-    }
-
-    public static void main(String[] args) {
-        new Solution().isMatch("mississippi", "mis*is*p*");
+        return ans;
     }
 }
