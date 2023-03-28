d4128bf0aa74fcc06026491059362a0dc84e116b
yanglongqi <yanglongqi@xiaomi.com>
2023-03-28 11:01:04 +0800

    [复习][15]三数之和

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
 
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int ans = 0;
-        while (left < right) {
-            ans = Math.max((right - left) * Math.min(height[left], height[right]), ans);
-            if (height[left] <= height[right]) {
-                left++;
-            } else {
-                right--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (left != i + 1 && nums[left] == nums[left - 1]) {
+                    left++;
+                    continue;
+                }
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    ArrayList<Integer> t = new ArrayList<>();
+                    t.add(nums[i]);
+                    t.add(nums[left]);
+                    t.add(nums[right]);
+                    ans.add(t);
+                    left++;
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
             }
 
         }
+
         return ans;
     }
 }
