5e0d3487edf321d175e7f421a8337ac20069fb80
yanglongqi <yanglongqi@xiaomi.com>
2023-03-31 12:19:57 +0800

    [复习][34]在排序数组中查找元素的第一个和最后一个位置

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,30 @@
 class Solution {
 
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{binarySearch(nums, target, true), binarySearch(nums, target, false)};
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (target == nums[mid]) {
-                return mid;
-            }
-
-            if (nums[mid] >= nums[left] && target >= nums[left] && target <= nums[mid]) {
-                right = mid - 1;
-            } else if (nums[mid] < nums[left] && (target <= nums[mid] || target >= nums[left])) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return -1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().search(new int[]{5, 1, 3}, 5));
+    public int binarySearch(int[] nums, int target, boolean isLower) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                if (isLower) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return ans;
     }
 }
