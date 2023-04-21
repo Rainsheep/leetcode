fe4d78218aad82a0f7fedf4bccf7077b3c5d30d2
yanglongqi <yanglongqi@xiaomi.com>
2023-04-21 10:44:47 +0800

    [复习][45]跳跃游戏 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
-    public boolean isMatch(String s, String p) {
-        int sLen = s.length();
-        int pLen = p.length();
-        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
-        // i==0
-        for (int i = 1; i <= pLen; i++) {
-            if (p.charAt(i - 1) == '*') {
-                dp[0][i] = true;
-            } else {
-                break;
+
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int step = 0;
+        int nowMax = 0;
+        int nextMax = nums[0];
+        for (int i = 0; i < n; i++) {
+            if (nowMax >= nums.length - 1) {
+                return step;
             }
-        }
-        // j==0
-        for (int i = 1; i <= sLen; i++) {
-            dp[i][0] = false;
-        }
-        dp[0][0] = true;
-        for (int i = 1; i <= sLen; i++) {
-            for (int j = 1; j <= pLen; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j - 1] || dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] && isMatch(s.charAt(i - 1), p.charAt(j - 1));
-                }
+            nextMax = Math.max(nextMax, i + nums[i]);
+            if (i == nowMax) {
+                step++;
+                nowMax = nextMax;
             }
+
         }
-
-        // print(dp);
-
-        return dp[sLen][pLen];
-
+        return step;
     }
 
-    private boolean isMatch(char a, char b) {
-        return b == '?' || b == '*' || a == b;
-    }
-
-    private void print(boolean[][] arr) {
-        for (boolean[] booleans : arr) {
-            System.out.println(Arrays.toString(booleans));
-        }
+    public static void main(String[] args) {
+        new Solution().jump(new int[]{1, 1, 1, 1});
     }
 }
