ecf955bcf9bc88bea94471c4da3c562e469d247d
Rainsheep <1045674971@qq.com>
2023-06-08 23:24:28 +0800

    [复习][140]单词拆分 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,24 @@
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
 
-    public int singleNumber(int[] nums) {
-        int res = 0;
-        for (int num : nums) {
-            res ^= num;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        int length = s.length();
+        boolean[] dp = new boolean[length + 1];
+        dp[0] = true;
+        for (int i = 1; i <= length; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = dp[j] && set.contains(s.substring(j, i));
+                if (dp[i]) {
+                    break;
+                }
+            }
         }
-        return res;
-
+        return dp[length];
     }
+
+
 }
+
