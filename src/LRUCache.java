c30c2b9d62789fe22d6063fa05f2cd36d2dbc5ae
Rainsheep <1045674971@qq.com>
2023-06-10 02:08:43 +0800

    [复习][146]LRU 缓存

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -1,26 +1,86 @@
-import java.util.LinkedHashMap;
-import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Objects;
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache {
+
+    static class Node<K, V> {
+
+        K key;
+        V value;
+        Node<K, V> pre;
+        Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> pre, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.pre = pre;
+            this.next = next;
+        }
+    }
 
     int capacity;
 
+    HashMap<Integer, Node<Integer, Integer>> map;
+    Node<Integer, Integer> head;
+    Node<Integer, Integer> tail;
+
     public LRUCache(int capacity) {
-        super(capacity, 0.75f, true);
+        map = new HashMap<>();
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        return getOrDefault(key, -1);
+        Node<Integer, Integer> node = map.get(key);
+        if (node != null) {
+            move2Tail(node);
+            return node.value;
+        }
+        return -1;
     }
 
-    @Override
-    protected boolean removeEldestEntry(Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    public void put(Integer key, Integer value) {
+        if (map.containsKey(key)) {
+            Node<Integer, Integer> node = map.get(key);
+            node.value = value;
+            move2Tail(node);
+        } else {
+            Node<Integer, Integer> node = new Node<>(key, value, null, null);
+            map.put(key, node);
+            add2Tail(node);
+
+            if (map.size() > capacity) {
+                map.remove(head.key);
+                head = head.next;
+            }
+        }
     }
+
+    private void move2Tail(Node<Integer, Integer> node) {
+        if (Objects.equals(node.key, tail.key)) {
+            return;
+        }
+
+        if (Objects.equals(node.key, head.key)) {
+            head = head.next;
+            add2Tail(node);
+            return;
+        }
+
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+        add2Tail(node);
+
+    }
+
+    private void add2Tail(Node<Integer, Integer> node) {
+        node.pre = tail;
+        if (tail != null) {
+            tail.next = node;
+        }
+        tail = node;
+        if (head == null) {
+            head = node;
+        }
+    }
+
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such: LRUCache obj = new LRUCache(capacity); int param_1 =
- * obj.get(key); obj.put(key,value);
- */
