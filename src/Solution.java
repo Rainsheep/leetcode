a793e04c677aa8e82348c2491acca6c1437b00f4
Rainsheep <1045674971@qq.com>
2023-06-09 23:47:28 +0800

    [复习][142]环形链表 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,37 @@
-import java.util.HashSet;
-import java.util.List;
 
-class Solution {
+class ListNode {
 
-    public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> set = new HashSet<>(wordDict);
-        int length = s.length();
-        boolean[] dp = new boolean[length + 1];
-        dp[0] = true;
-        for (int i = 1; i <= length; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] = dp[j] && set.contains(s.substring(j, i));
-                if (dp[i]) {
-                    break;
-                }
-            }
-        }
-        return dp[length];
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
-
-
 }
 
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+
+    }
+}
