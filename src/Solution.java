02072a7751a052c3f06758d3f17bd0e22f1a3c53
Rainsheep <1045674971@qq.com>
2023-06-12 22:43:24 +0800

    [复习][207]课程表

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,44 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int[] buy = new int[k];
-        int[] sell = new int[k];
-        int ans = 0;
-        Arrays.fill(buy, Integer.MIN_VALUE);
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] outCount = new int[numCourses];
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        int count = 0;
 
+        for (int[] prerequisite : prerequisites) {
+            outCount[prerequisite[0]]++;
+            HashSet<Integer> set = map.computeIfAbsent(prerequisite[1], k -> new HashSet<>());
+            set.add(prerequisite[0]);
+        }
 
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = 0; j < k && j <= i; j++) {
-                buy[j] = Math.max((j == 0 ? 0 : sell[j - 1]) - prices[i], buy[j]);
-                sell[j] = Math.max(buy[j] + prices[i], sell[j]);
-                ans = Math.max(ans, sell[j]);
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (outCount[i] == 0) {
+                queue.add(i);
             }
         }
-        return ans;
+
+        while (true) {
+            if (queue.isEmpty()) {
+                return count == numCourses;
+            }
+
+            Integer head = queue.pop();
+            HashSet<Integer> set = map.get(head);
+            if (set != null && set.size() != 0) {
+                for (Integer num : set) {
+                    outCount[num]--;
+                    if (outCount[num] == 0) {
+                        queue.add(num);
+                    }
+                }
+            }
+            count++;
+
+        }
 
     }
 }
