c786d93a41c721d89816edfcee757c6dd4206d75
Rainsheep <1045674971@qq.com>
2023-06-12 22:09:28 +0800

    [复习][188]买卖股票的最佳时机 IV

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
-    public int majorityElement(int[] nums) {
-        int count = 0;
-        int res = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                res = num;
-                count = 1;
-                continue;
+    public int maxProfit(int k, int[] prices) {
+        int[] buy = new int[k];
+        int[] sell = new int[k];
+        int ans = 0;
+        Arrays.fill(buy, Integer.MIN_VALUE);
+
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 0; j < k && j <= i; j++) {
+                buy[j] = Math.max((j == 0 ? 0 : sell[j - 1]) - prices[i], buy[j]);
+                sell[j] = Math.max(buy[j] + prices[i], sell[j]);
+                ans = Math.max(ans, sell[j]);
             }
-            count += num == res ? 1 : -1;
         }
-        return res;
+        return ans;
+
     }
 }
