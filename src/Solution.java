92901b4e58aa988e5ac2830350287cdae812a0cd
Rainsheep <1045674971@qq.com>
2023-06-13 23:08:05 +0800

    [复习][238]除自身以外数组的乘积

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int n = matrix[0].length;
-        int m = matrix.length;
-        int[][] dp = new int[m][n];
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    dp[i][j] = (i == 0 || j == 0) ? 1 : (Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1);
-                    ans = Math.max(dp[i][j], ans);
-                }
-            }
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = i == 0 ? 1 : (res[i - 1] * nums[i - 1]);
         }
-        return ans * ans;
+
+        int R = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = R * res[i];
+            R *= nums[i];
+        }
+
+        return res;
 
     }
 }
