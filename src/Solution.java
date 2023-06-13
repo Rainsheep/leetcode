388c13291c68246d2b7e188f433c3e8dd5559bf5
Rainsheep <1045674971@qq.com>
2023-06-13 22:40:26 +0800

    [复习][221]最大正方形

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,18 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix[0].length;
         int m = matrix.length;
-        int[][] num = new int[m][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                num[j][i] = matrix[j][i] == '1' ? ((j > 0 ? num[j - 1][i] : 0) + 1) : 0;
-            }
-        }
-
+        int[][] dp = new int[m][n];
         int ans = 0;
         for (int i = 0; i < m; i++) {
-            int[] left = new int[n];
-            int[] right = new int[n];
-            Arrays.fill(right, n - 1);
-            LinkedList<Integer> queue = new LinkedList<>();
             for (int j = 0; j < n; j++) {
-                while (!queue.isEmpty() && num[i][queue.getLast()] >= num[i][j]) {
-                    Integer index = queue.removeLast();
-                    right[index] = j - 1;
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = (i == 0 || j == 0) ? 1 : (Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1);
+                    ans = Math.max(dp[i][j], ans);
                 }
-
-                left[j] = queue.isEmpty() ? 0 : (queue.getLast() + 1);
-                queue.addLast(j);
-            }
-//            System.out.println(Arrays.toString(right));
-
-            for (int j = 0; j < n; j++) {
-                int width = right[j] - left[j] + 1;
-                int l = Math.min(width, num[i][j]);
-                ans = Math.max(ans, l * l);
             }
         }
-
-        return ans;
+        return ans * ans;
 
     }
 }
