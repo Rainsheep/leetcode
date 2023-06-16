423837b6a5320e38e06993398e1c5fd22bf8d8ed
Rainsheep <1045674971@qq.com>
2023-06-16 23:46:31 +0800

    [复习][301]删除无效的括号

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,49 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            if (list.isEmpty() || num > list.get(list.size() - 1)) {
-                list.add(num);
+    int ans = 0;
+    HashSet<String> set = new HashSet<>();
+
+    public List<String> removeInvalidParentheses(String s) {
+        dfs(s, 0, 0, 0, new StringBuilder());
+        return new ArrayList<>(set);
+
+    }
+
+    private void dfs(String s, int index, int left, int right, StringBuilder sb) {
+        if (s.length() == index) {
+            if (left == right) {
+                if (sb.length() == ans) {
+                    set.add(sb.toString());
+                } else if (sb.length() > ans) {
+                    ans = sb.length();
+                    set.clear();
+                    set.add(sb.toString());
+                }
             }
-            if (num == list.get(list.size() - 1)) {
-                continue;
-            }
-            int index = Collections.binarySearch(list, num);
-            if (index < 0) {
-                index = -index - 1;
-            }
-            list.set(index, num);
+            return;
         }
-        return list.size();
+
+        char c = s.charAt(index);
+        if (c != '(' && c != ')') {
+            sb.append(c);
+            dfs(s, index + 1, left, right, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        } else if (c == '(') {
+            sb.append(c);
+            dfs(s, index + 1, left + 1, right, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        } else if (right < left) {
+            sb.append(c);
+            dfs(s, index + 1, left, right + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        dfs(s, index + 1, left, right, sb);
+    }
+
+    public static void main(String[] args) {
+        new Solution().removeInvalidParentheses(")(f");
     }
 }
