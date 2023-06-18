c8e05b74ba45df601db229cb5b9b4297c7eeb03e
Rainsheep <1045674971@qq.com>
2023-06-18 17:07:24 +0800

    [复习][347]前 K 个高频元素

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,24 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 class Solution {
-    public int[] countBits(int n) {
-        int len = n + 1;
-        int[] nums = new int[len];
-        nums[0] = 0;
-        int k = 1;
-        int r = 0;
-        while (k < len) {
-            int pow = (int) Math.pow(2, r);
-            for (int i = 0; i < pow && k < len; i++) {
-                if (i < pow / 2) {
-                    nums[k] = nums[k - pow / 2];
-                } else {
-                    nums[k] = nums[k - pow / 2] + 1;
-                }
-                k++;
-            }
-            r++;
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        return nums;
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        queue.addAll(map.entrySet());
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll().getKey();
+        }
+        return ans;
+
 
     }
 }
