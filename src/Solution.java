08423652f21577bfe0068537b7550ee107529347
Rainsheep <1045674971@qq.com>
2023-06-27 00:19:33 +0800

    [复习][581]最短无序连续子数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,56 @@
-import java.util.HashMap;
-
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        int ans = 0;
-        int preSum = 0;
-        for (int num : nums) {
-            preSum += num;
-            ans += map.getOrDefault(preSum - k, 0);
-            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+    public int findUnsortedSubarray(int[] nums) {
+        int left = -1;
+        int length = nums.length;
+        for (int i = 0; i < length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                left = i;
+                break;
+            }
         }
-        return ans;
+        if (left == -1) {
+            return 0;
+        }
+
+        int right = -1;
+        for (int i = length - 1; i > 0; i--) {
+            if (nums[i] < nums[i - 1]) {
+                right = i;
+                break;
+            }
+        }
+
+        int min = left;
+        int max = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] < nums[min]) {
+                min = i;
+            }
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+        int leftIndex = left;
+
+        for (int i = left; i >= 0; i--) {
+            if (nums[i] > nums[min]) {
+                leftIndex--;
+            } else {
+                break;
+            }
+        }
+
+        int rightIndex = right;
+        for (int i = right; i < length; i++) {
+            if (nums[i] < nums[max]) {
+                rightIndex++;
+            } else {
+                break;
+            }
+        }
+
+        return rightIndex - leftIndex + 1;
     }
+
+
 }
