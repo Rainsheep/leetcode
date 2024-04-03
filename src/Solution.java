9faa3d4c703e5e49779a62f57d816a4099928203
yanglongqi <yanglongqi@xiaomi.com>
2024-04-03 16:42:20 +0800

    [3]无重复字符的最长子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,22 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 class Solution {
-
-    public int countSubstrings(String s) {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
         int ans = 0;
-        s = wrapperStr(s);
-        int cid = -1, right = -1;
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        for (int i = 1; i < s.length() - 1; i++) {
-            int armLen = i <= right ? Math.min(list.get(2 * cid - i), right - i + 1) : 1;
-            while (s.charAt(i + armLen) == s.charAt(i - armLen)) {
-                armLen++;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                left = Math.max(map.get(c) + 1, left);
             }
-            list.add(armLen);
-
-            if (i + armLen - 1 > right) {
-                cid = i;
-                right = i + armLen - 1;
-            }
-
-            // System.out.println(i + " " + armLen / 2);
-            ans += armLen / 2;
+            ans = Math.max(ans, i - left + 1);
+            map.put(c, i);
         }
-
         return ans;
     }
 
-    public String wrapperStr(String s) {
-        StringBuilder sb = new StringBuilder("^#");
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-            sb.append("#");
-        }
-        sb.append("$");
-        return sb.toString();
-    }
-
     public static void main(String[] args) {
-        System.out.println(new Solution().countSubstrings("aba"));
+        new Solution().lengthOfLongestSubstring("abba");
     }
 }
