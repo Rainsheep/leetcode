302125ccbdc6c9d8b109208bab82c17133f1d935
yanglongqi <yanglongqi@xiaomi.com>
2024-05-10 19:59:28 +0800

    [146]LRU 缓存

---
+++ src/LRUCache.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return getOrDefault(key, -1);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,25 @@
 class Solution {
+    public int longestValidParentheses(String s) {
+        int res = 0;
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left >= right) {
+                res = Math.max(res, right * 2);
+            } else {
+                left = 0;
+                right = 0;
+            }
+        }
+        return res;
+
+    }
 }
