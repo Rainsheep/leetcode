6f12712a45921c1ae9c080ed15c1e2fac3df585d
yanglongqi <yanglongqi@xiaomi.com>
2024-05-11 10:12:28 +0800

    [复习][146]LRU 缓存

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,89 @@
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache {
+
+    static class ListNode {
+        int key;
+        int val;
+        ListNode pre;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    Map<Integer, ListNode> map;
 
     int capacity;
 
-    public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
-        this.capacity = capacity;
+    ListNode head;
+    ListNode tail;
 
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        this.capacity = capacity;
+        head = new ListNode();
+        tail = new ListNode();
+        head.next = tail;
+        tail.pre = head;
     }
 
     public int get(int key) {
-        return getOrDefault(key, -1);
+        if (map.containsKey(key)) {
+            moveToHead(map.get(key));
+            return map.get(key).val;
+        }
+        return -1;
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    private void moveToHead(ListNode listNode) {
+        listNode.pre.next = listNode.next;
+        listNode.next.pre = listNode.pre;
+        addToHead(listNode);
     }
+
+    private void addToHead(int key, int val) {
+        addToHead(new ListNode(key, val));
+    }
+
+    private void addToHead(ListNode listNode) {
+        map.put(listNode.key, listNode);
+        listNode.next = head.next;
+        listNode.pre = head;
+        head.next.pre = listNode;
+        head.next = listNode;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.get(key).val = value;
+            moveToHead(map.get(key));
+        } else if (map.size() < capacity) {
+            addToHead(key, value);
+        } else {
+            addToHead(key, value);
+            removeTail();
+
+        }
+
+    }
+
+    private void removeTail() {
+        ListNode realTail = tail.pre;
+        removeNode(realTail);
+        map.remove(realTail.key);
+    }
+
+    private void removeNode(ListNode node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
 }
+
