66652ef0d17d9c5d006e60c71cefa7b2d8ee8165
yanglongqi <yanglongqi@xiaomi.com>
2024-05-13 20:05:18 +0800

    [494]目标和

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    int ans = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        dfs(nums, target, 0, 0);
+        return ans;
+    }
+
+    private void dfs(int[] nums, int target, int sum, int index) {
+        if (index == nums.length) {
+            ans += (sum == target) ? 1 : 0;
+            return;
+        }
+
+        dfs(nums, target, sum + nums[index], index + 1);
+        dfs(nums, target, sum - nums[index], index + 1);
     }
 }
