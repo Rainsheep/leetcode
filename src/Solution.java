997091151c9e0ca916510a1fda952263f90b0cad
yanglongqi <yanglongqi@xiaomi.com>
2024-05-13 19:51:41 +0800

    [226]翻转二叉树

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,12 @@
 class Solution {
-    public int longestValidParentheses(String s) {
-        int[] dp = new int[s.length()];
-        int max = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                continue;
-            }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
 
-            if (s.charAt(i - 1) == '(') {
-                dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-            } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
-                dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0);
-            }
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+        TreeNode t;
+        t = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(t);
+        return root;
+
     }
 }
