64d3837fe2200f52218aa8adad1b204472769181
yanglongqi <yanglongqi@xiaomi.com>
2024-06-12 12:59:52 +0800

    [1019]链表中的下一个更大节点

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,31 @@
-import java.util.StringJoiner;
+import java.util.ArrayList;
 
 class Solution {
-    public String reverseWords(String s) {
-        s = s.trim();
-        String[] split = s.split("\\s+");
-        StringJoiner stringJoiner = new StringJoiner(" ");
-        for (int i = split.length - 1; i >= 0; i--) {
-            stringJoiner.add(split[i]);
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
         }
-        return stringJoiner.toString();
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (i > 0 && list.get(i) <= list.get(i - 1)) {
+                ans[i] = ans[i - 1];
+            }
+
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) > list.get(i)) {
+                    ans[i] = list.get(j);
+                    break;
+                }
+            }
+        }
+
+
+        return ans;
+
     }
 }
