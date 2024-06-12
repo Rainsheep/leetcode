f3e545762ebc4c3357eaba5713f73d7b3ae266bc
yanglongqi <yanglongqi@xiaomi.com>
2024-06-12 12:41:36 +0800

    [917]仅仅反转字母

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,26 @@
-import java.util.Random;
-
 class Solution {
-    public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-        return nums;
-    }
-
-    private void quickSort(int[] nums, int low, int high) {
-        if (low < high) {
-            int k = partition(nums, low, high);
-            quickSort(nums, low, k - 1);
-            quickSort(nums, k + 1, high);
-        }
-    }
-
-    private int partition(int[] nums, int low, int high) {
-        int random = new Random().nextInt(high - low);
-        swap(nums, low, low + random);
-        int x = nums[low];
-        int k = low - 1;
-        for (int i = low; i <= high; i++) {
-            if (nums[i] <= x) {
-                swap(nums, i, ++k);
+    public String reverseOnlyLetters(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetter(chars[left])) {
+                left++;
             }
+
+            while (left < right && !Character.isLetter(chars[right])) {
+                right--;
+            }
+            swap(chars, left++, right--);
         }
-        swap(nums, k, low);
-        return k;
+
+        return new String(chars);
+
     }
 
-    private void swap(int[] nums, int i, int j) {
-        if (i != j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+    private void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
